b1859dd53c3d8bea1d413cf52fb37ee31d1fde54
tdegeus <tom@geus.me>
2017-11-01 09:52:57 +0100

    Added 1d dynamics example

---
+++ docs/examples/theory/Dynamics_diagonalMass_1d/damping.py
@@ -0,0 +1,79 @@
+
+import example
+import numpy             as np
+import matplotlib        as mpl
+import matplotlib.pyplot as plt
+
+plt.style.use(['goose','goose-latex'])
+
+fig,ax = plt.subplots()
+
+# set variation in "nu" (critical damping at, nu == 1); colorbar settings
+exp  = np.linspace(-3,+3,7)
+bnd  = np.linspace(-3,+4,8)-.5
+lab  = ['1/%d'%2**(-i) for i in exp if i<0] + ['%d'%2**i for i in exp if i>=0]
+nus  = np.array([2**i for i in exp])
+cmap = plt.get_cmap('viridis',len(nus))
+
+# loop over all values of "nu"
+for inu,nu in enumerate(nus):
+
+  # set all parameters
+  nelem = 100
+  c     = np.sqrt(nu)
+  G     = 1.
+  rho   = G / c**2.
+  eta   = nu * rho
+  L     = np.pi
+  h     = L / nelem
+  qh    = 2. * np.pi / h
+  qL    = 2. * np.pi / L
+
+  # set external force: point force on the middle node
+  imid = int(nelem/2)
+  Fext = np.zeros((nelem+1)); Fext[imid] = -0.001 * G * h
+
+  # get time-scale : different from underdamped and overdamped systems
+  if ( nu/c < 2./qL ):
+    dt  = 1 / ( c * qh )
+    dt /= 10.
+  else:
+    omega1 = ((nu*qh**2.)/2.) * np.sqrt( 2. - (2.*c/(qh*nu))**2. + np.sqrt(1.-(2.*c/(qh*nu))**2.) )
+    omega2 = ((nu*qh**2.)/2.) * np.sqrt( 2. - (2.*c/(qh*nu))**2. - np.sqrt(1.-(2.*c/(qh*nu))**2.) )
+    dt     = max( 1./omega1 , 1./omega2 )
+
+  # set number of increments (total time constant for all samples)
+  T    = 50.
+  ninc = int(T/dt)
+
+  # run simulation
+  u = example.compute(
+    rho        = rho,
+    G          = G,
+    eta        = eta,
+    h          = h,
+    Fext       = Fext,
+    dt         = dt,
+    ninc       = ninc,
+    save_every = 1,
+    save_nodes = [imid],
+  )
+
+  # plot response
+  ax.plot(np.linspace(0,ninc*dt,len(u)),u/L,color=cmap(inu))
+
+# set plot labels
+plt.xlabel(r'$t$')
+plt.ylabel(r'$u$')
+
+# add colormap
+norm = mpl.colors.Normalize(vmin=min(exp),vmax=max(exp))
+sm   = plt.cm.ScalarMappable(cmap=cmap,norm=norm)
+sm.set_array([])
+cbar = plt.colorbar(sm,ticks=exp,boundaries=bnd)
+cbar.set_ticklabels(lab)
+
+# show/save
+plt.show()
+plt.savefig('damping.svg')
+
---
+++ docs/examples/theory/Dynamics_diagonalMass_1d/geometry.py
@@ -0,0 +1,62 @@
+
+import example
+import numpy             as np
+import matplotlib        as mpl
+import matplotlib.pyplot as plt
+
+# set all parameters
+nelem = 100
+nu    = 1./8.
+c     = np.sqrt(nu)
+G     = 1.
+rho   = G / c**2.
+eta   = nu * rho
+L     = np.pi
+h     = L / nelem
+qh    = 2. * np.pi / h
+qL    = 2. * np.pi / L
+
+# set external force: point force on the middle node
+imid = int(nelem/2)
+Fext = np.zeros((nelem+1)); Fext[imid] = -0.001 * G * h
+
+# get time-scale : different from underdamped and overdamped systems
+if ( nu/c < 2./qL ):
+  dt  = 1 / ( c * qh )
+  dt /= 10.
+else:
+  omega1 = ((nu*qh**2.)/2.) * np.sqrt( 2. - (2.*c/(qh*nu))**2. + np.sqrt(1.-(2.*c/(qh*nu))**2.) )
+  omega2 = ((nu*qh**2.)/2.) * np.sqrt( 2. - (2.*c/(qh*nu))**2. - np.sqrt(1.-(2.*c/(qh*nu))**2.) )
+  dt     = max( 1./omega1 , 1./omega2 )
+
+# set number of increments (total time constant for all samples)
+T    = 50.
+ninc = int(T/dt)
+
+# run simulation
+u = example.compute(
+  rho        = rho,
+  G          = G,
+  eta        = eta,
+  h          = h,
+  Fext       = Fext,
+  dt         = dt,
+  ninc       = ninc,
+  save_every = 1,
+  save_nodes = np.arange(nelem+1),
+)
+
+# plot response
+for i in range(u.shape[0])[::10]:
+
+  fig,ax = plt.subplots()
+  ax.plot(np.linspace(0,L,nelem+1),u[i,:]/L,color='k')
+
+  ax.axes.get_xaxis().set_visible(False)
+  ax.axes.get_yaxis().set_visible(False)
+
+  plt.xlim([0,L])
+  plt.ylim([-0.000012,0.000002])
+
+  plt.savefig('geometry_%05d.svg'%i)
+  plt.close()
