a2c08147a0efb1e0dc02f8fc8c6635b082d0f9d5
Tom de Geus <tom@geus.me>
2018-04-24 15:15:37 +0200

    Update second example

--- docs/examples/DynamicsDiagonal/Periodic/laminate/no_damping/velocityVerlet/plot.py
+++ docs/examples/DynamicsDiagonal/Periodic/laminate/no_damping/velocityVerlet/plot.py
@@ -1,10 +1,10 @@
 
-import sys,os,re
+import sys, os, re
 import h5py
-import numpy as np
+import numpy             as np
 import matplotlib.pyplot as plt
-import goosempl as gplt
-import matplotlib as mpl
+import goosempl          as gplt
+import matplotlib        as mpl
 
 plt.style.use(['goose','goose-latex'])
 
@@ -13,38 +13,38 @@ plt.style.use(['goose','goose-latex'])
 name = 'example.hdf5'
 f    = h5py.File(name,'r')
 
-fig = plt.figure(figsize=(20,10))
-fig.set_tight_layout(True)
+fig, axes = plt.subplots(figsize=(20,10), ncols=2)
 
 # --------------------------------------------------------------------------------------------------
 
-ax = fig.add_subplot(1,2,1)
+ax = axes[0]
 
 t = f['/global/t'   ][:]
 E = f['/global/Ekin'][:] + f['/global/Epot'][:]; ax.plot(t,E,color='k',label=r'$E_\mathrm{pot} + E_\mathrm{kin}$')
 E = f['/global/Epot'][:]                       ; ax.plot(t,E,color='r',label=r'$E_\mathrm{pot}$')
 E = f['/global/Ekin'][:]                       ; ax.plot(t,E,color='b',label=r'$E_\mathrm{kin}$')
 
-plt.legend(loc='lower right')
+ax.legend(loc='lower right')
 
-plt.xlabel(r'$t$')
-plt.ylabel(r'$E$')
+ax.set_xlabel(r'$t$')
+ax.set_ylabel(r'$E$')
 
 # --------------------------------------------------------------------------------------------------
 
-ax = fig.add_subplot(1,2,2)
+ax = axes[1]
 
 coor = f['/mesh/coor'][:]
 conn = f['/mesh/conn'][:]
 u    = f['/mesh/disp'][:]
+
 
 im = gplt.patch(coor=coor+u,conn=conn)
 
 Lx = np.max(coor[:,0])-np.min(coor[:,0])
 Ly = np.max(coor[:,1])-np.min(coor[:,1])
 
-plt.xlim([-0.1*Lx,1.3*Lx])
-plt.ylim([-0.1*Ly,1.3*Ly])
+ax.set_xlim([-0.1*Lx,1.3*Lx])
+ax.set_ylim([-0.1*Ly,1.3*Ly])
 
 ax.set_aspect('equal')
 
