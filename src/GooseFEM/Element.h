d75f819fa34dd97098b47e8df4dd971a408d2621
Tom de Geus <tom@geus.me>
2018-07-27 14:37:55 +0200

    Changed to xtensor: Element

--- src/GooseFEM/Element.h
+++ src/GooseFEM/Element.h
@@ -19,10 +19,10 @@ namespace Element {
 // -------------------------------------------------------------------------------------------------
 
 // convert nodal vector [nnode, ndim] to nodal vector stored per element [nelem, nne, ndim]
-inline ArrD asElementVector(const xt::xtensor<size_t,2> &conn, const xt::xtensor<double,2> &nodevec);
+inline xt::xtensor<double,3> asElementVector(const xt::xtensor<size_t,2> &conn, const xt::xtensor<double,2> &nodevec);
 
 // assemble nodal vector stored per element [nelem, nne, ndim] to nodal vector [nnode, ndim]
-inline xt::xtensor<double,2> assembleNodeVector(const xt::xtensor<size_t,2> &conn, const ArrD &elemvec);
+inline xt::xtensor<double,2> assembleNodeVector(const xt::xtensor<size_t,2> &conn, const xt::xtensor<double,3> &elemvec);
 
 // -------------------------------------------------------------------------------------------------
 
