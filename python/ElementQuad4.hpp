728d6ed39dd132f02e19553b986758a6b2807db7
Tom de Geus <tom@geus.me>
2019-10-06 17:46:46 +0200

    Code-formatting

--- python/ElementQuad4.hpp
+++ python/ElementQuad4.hpp
@@ -24,27 +24,80 @@ void init_ElementQuad4(py::module &m)
 
 py::class_<GooseFEM::Element::Quad4::Quadrature>(m, "Quadrature")
 
-  .def(py::init<const xt::xtensor<double,3> &>(), "Quadrature", py::arg("x"))
+  .def(py::init<const xt::xtensor<double,3>&>(),
+    "Quadrature",
+    py::arg("x"))
 
-  .def(py::init<const xt::xtensor<double,3> &, const xt::xtensor<double,2> &, const xt::xtensor<double,1> &>(), "Quadrature", py::arg("x"), py::arg("xi"), py::arg("w"))
+  .def(py::init<const xt::xtensor<double,3>&,
+                const xt::xtensor<double,2>&,
+                const xt::xtensor<double,1>&>(),
+    "Quadrature",
+    py::arg("x"),
+    py::arg("xi"),
+    py::arg("w"))
 
-  .def("nelem", &GooseFEM::Element::Quad4::Quadrature::nelem, "Return number of elements"          )
-  .def("nne"  , &GooseFEM::Element::Quad4::Quadrature::nne  , "Return number of nodes per element" )
-  .def("ndim" , &GooseFEM::Element::Quad4::Quadrature::ndim , "Return number of dimensions"        )
-  .def("nip"  , &GooseFEM::Element::Quad4::Quadrature::nip  , "Return number of integration points")
+  .def("nelem",
+    &GooseFEM::Element::Quad4::Quadrature::nelem,
+    "Number of elements")
 
-  .def("DV", py::overload_cast<size_t>(&GooseFEM::Element::Quad4::Quadrature::DV, py::const_), "Integration point volume (qtensor)")
-  .def("DV", py::overload_cast<>      (&GooseFEM::Element::Quad4::Quadrature::DV, py::const_), "Integration point volume (qscalar)")
+  .def("nne",
+    &GooseFEM::Element::Quad4::Quadrature::nne,
+    "Number of nodes per element")
 
-  .def("GradN_vector"   , py::overload_cast<const xt::xtensor<double,3> &>(&GooseFEM::Element::Quad4::Quadrature::GradN_vector   , py::const_), "Dyadic product, returns 'qtensor'", py::arg("elemvec"))
-  .def("GradN_vector_T" , py::overload_cast<const xt::xtensor<double,3> &>(&GooseFEM::Element::Quad4::Quadrature::GradN_vector_T , py::const_), "Dyadic product, returns 'qtensor'", py::arg("elemvec"))
-  .def("SymGradN_vector", py::overload_cast<const xt::xtensor<double,3> &>(&GooseFEM::Element::Quad4::Quadrature::SymGradN_vector, py::const_), "Dyadic product, returns 'qtensor'", py::arg("elemvec"))
+  .def("ndim",
+    &GooseFEM::Element::Quad4::Quadrature::ndim,
+    "Number of dimensions")
 
-  .def("Int_N_scalar_NT_dV"                 , py::overload_cast<const xt::xtensor<double,2> &>(&GooseFEM::Element::Quad4::Quadrature::Int_N_scalar_NT_dV                 , py::const_), "Integration, returns 'elemmat'", py::arg("qscalar"))
-  .def("Int_gradN_dot_tensor2_dV"           , py::overload_cast<const xt::xtensor<double,4> &>(&GooseFEM::Element::Quad4::Quadrature::Int_gradN_dot_tensor2_dV           , py::const_), "Integration, returns 'elemvec'", py::arg("qtensor"))
-  .def("Int_gradN_dot_tensor4_dot_gradNT_dV", py::overload_cast<const xt::xtensor<double,6> &>(&GooseFEM::Element::Quad4::Quadrature::Int_gradN_dot_tensor4_dot_gradNT_dV, py::const_), "Integration, returns 'elemvec'", py::arg("qtensor"))
+  .def("nip",
+    &GooseFEM::Element::Quad4::Quadrature::nip,
+    "Number of integration points")
 
-  .def("__repr__", [](const GooseFEM::Element::Quad4::Quadrature &){ return "<GooseFEM.Element.Quad4.Quadrature>"; });
+  .def("DV",
+    py::overload_cast<size_t>(&GooseFEM::Element::Quad4::Quadrature::DV, py::const_),
+    "Integration point volume (qtensor)")
+
+  .def("DV",
+    py::overload_cast<>(&GooseFEM::Element::Quad4::Quadrature::DV, py::const_),
+    "Integration point volume (qscalar)")
+
+  .def("GradN_vector",
+    py::overload_cast<const xt::xtensor<double,3>&>(
+      &GooseFEM::Element::Quad4::Quadrature::GradN_vector   , py::const_),
+    "Dyadic product, returns 'qtensor'",
+    py::arg("elemvec"))
+
+  .def("GradN_vector_T",
+    py::overload_cast<const xt::xtensor<double,3>&>(
+      &GooseFEM::Element::Quad4::Quadrature::GradN_vector_T , py::const_),
+    "Dyadic product, returns 'qtensor'",
+    py::arg("elemvec"))
+
+  .def("SymGradN_vector",
+    py::overload_cast<const xt::xtensor<double,3>&>(
+      &GooseFEM::Element::Quad4::Quadrature::SymGradN_vector, py::const_),
+    "Dyadic product, returns 'qtensor'",
+    py::arg("elemvec"))
+
+  .def("Int_N_scalar_NT_dV",
+    py::overload_cast<const xt::xtensor<double,2>&>(
+      &GooseFEM::Element::Quad4::Quadrature::Int_N_scalar_NT_dV, py::const_),
+    "Integration, returns 'elemmat'",
+    py::arg("qscalar"))
+
+  .def("Int_gradN_dot_tensor2_dV",
+    py::overload_cast<const xt::xtensor<double,4>&>(
+      &GooseFEM::Element::Quad4::Quadrature::Int_gradN_dot_tensor2_dV, py::const_),
+    "Integration, returns 'elemvec'",
+    py::arg("qtensor"))
+
+  .def("Int_gradN_dot_tensor4_dot_gradNT_dV",
+    py::overload_cast<const xt::xtensor<double,6>&>(
+      &GooseFEM::Element::Quad4::Quadrature::Int_gradN_dot_tensor4_dot_gradNT_dV, py::const_),
+    "Integration, returns 'elemvec'",
+    py::arg("qtensor"))
+
+  .def("__repr__", [](const GooseFEM::Element::Quad4::Quadrature&){
+    return "<GooseFEM.Element.Quad4.Quadrature>";});
 
 }
 
