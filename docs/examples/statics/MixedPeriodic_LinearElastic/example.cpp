9fc3bb7fbf091e425841b1310b0876689fc55d1c
Tom de Geus <tdegeus@users.noreply.github.com>
2020-03-05 11:52:33 +0100

    Updating: docs/examples/statics/MixedPeriodic_LinearElastic (#63)

---
+++ docs/examples/statics/MixedPeriodic_LinearElastic/example.cpp
@@ -0,0 +1,145 @@
+#include <GooseFEM/GooseFEM.h>
+#include <GooseFEM/MatrixPartitioned.h>
+#include <GMatElastic/Cartesian3d.h>
+#include <highfive/H5Easy.hpp>
+
+int main()
+{
+    // mesh
+    // ----
+
+    // define mesh
+    GooseFEM::Mesh::Quad4::Regular mesh(5, 5);
+
+    // mesh dimensions
+    size_t nelem = mesh.nelem();
+    size_t nne = mesh.nne();
+    size_t ndim = mesh.ndim();
+
+    // mesh definitions
+    xt::xtensor<double,2> coor = mesh.coor();
+    xt::xtensor<size_t,2> conn = mesh.conn();
+    xt::xtensor<size_t,2> dofs = mesh.dofs();
+
+    // node sets
+    xt::xtensor<size_t,1> nodesLft = mesh.nodesLeftOpenEdge();
+    xt::xtensor<size_t,1> nodesRgt = mesh.nodesRightOpenEdge();
+    xt::xtensor<size_t,1> nodesTop = mesh.nodesTopEdge();
+    xt::xtensor<size_t,1> nodesBot = mesh.nodesBottomEdge();
+
+    // periodicity and fixed displacements DOFs
+    // ----------------------------------------
+
+    for (size_t j = 0; j < coor.shape(1); ++j) {
+        xt::view(dofs, xt::keep(nodesRgt), j) = xt::view(dofs, xt::keep(nodesLft), j);
+    }
+
+    dofs = GooseFEM::Mesh::renumber(dofs);
+
+    xt::xtensor<size_t,1> iip = xt::concatenate(xt::xtuple(
+        xt::view(dofs, xt::keep(nodesBot), 0),
+        xt::view(dofs, xt::keep(nodesBot), 1),
+        xt::view(dofs, xt::keep(nodesTop), 0),
+        xt::view(dofs, xt::keep(nodesTop), 1)));
+
+    // simulation variables
+    // --------------------
+
+    // vector definition
+    GooseFEM::VectorPartitioned vector(conn, dofs, iip);
+
+    // allocate system matrix
+    GooseFEM::MatrixPartitioned<> K(conn, dofs, iip);
+
+    // nodal quantities
+    xt::xtensor<double,2> disp = xt::zeros<double>(coor.shape());
+    xt::xtensor<double,2> fint = xt::zeros<double>(coor.shape());
+    xt::xtensor<double,2> fext = xt::zeros<double>(coor.shape());
+    xt::xtensor<double,2> fres = xt::zeros<double>(coor.shape());
+
+    // element vectors
+    xt::xtensor<double,3> ue = xt::empty<double>({nelem, nne, ndim});
+    xt::xtensor<double,3> fe = xt::empty<double>({nelem, nne, ndim});
+    xt::xtensor<double,3> Ke = xt::empty<double>({nelem, nne * ndim, nne * ndim});
+
+    // element/material definition
+    // ---------------------------
+
+    // element definition
+    GooseFEM::Element::Quad4::QuadraturePlanar elem(vector.AsElement(coor));
+    size_t nip = elem.nip();
+
+    // material definition
+    GMatElastic::Cartesian3d::Matrix mat(nelem, nip);
+    xt::xtensor<size_t,2> Ihard = xt::zeros<size_t>({nelem, nip});
+    xt::view(Ihard, xt::keep(0, 1, 5, 6), xt::all()) = 1;
+    xt::xtensor<size_t,2> Isoft = xt::ones<size_t>({nelem, nip}) - Ihard;
+    mat.setElastic(Isoft, 10.0, 1.0);
+    mat.setElastic(Ihard, 10.0, 10.0);
+
+    // integration point tensors
+    xt::xtensor<double,4> Eps = xt::empty<double>({nelem, nip, 3ul, 3ul});
+    xt::xtensor<double,4> Sig = xt::empty<double>({nelem, nip, 3ul, 3ul});
+    xt::xtensor<double,6> C = xt::empty<double>({nelem, nip, 3ul, 3ul, 3ul, 3ul});
+
+    // solve
+    // -----
+
+    // strain
+    vector.asElement(disp, ue);
+    elem.symGradN_vector(ue, Eps);
+
+    // stress & tangent
+    mat.tangent(Eps, Sig, C);
+
+    // internal force
+    elem.int_gradN_dot_tensor2_dV(Sig, fe);
+    vector.assembleNode(fe, fint);
+
+    // stiffness matrix
+    elem.int_gradN_dot_tensor4_dot_gradNT_dV(C, Ke);
+    K.assemble(Ke);
+
+    // set fixed displacements
+    xt::view(disp, xt::keep(nodesTop), 0) = +0.1;
+
+    // residual
+    xt::noalias(fres) = fext - fint;
+
+    // solve
+    K.solve(fres, disp);
+
+    // post-process
+    // ------------
+
+    // compute strain and stress
+    vector.asElement(disp, ue);
+    elem.symGradN_vector(ue, Eps);
+    mat.stress(Eps, Sig);
+
+    // internal force
+    elem.int_gradN_dot_tensor2_dV(Sig, fe);
+    vector.assembleNode(fe, fint);
+
+    // apply reaction force
+    vector.copy_p(fint, fext);
+
+    // residual
+    xt::noalias(fres) = fext - fint;
+
+    // print residual
+    std::cout << xt::sum(xt::abs(fres))[0] / xt::sum(xt::abs(fext))[0] << std::endl;
+
+    // average stress per node
+    xt::xtensor<double,4> dV = elem.DV(2);
+    xt::xtensor<double,3> SigAv = xt::average(Sig, dV, {1});
+
+    // write output
+    H5Easy::File file("output.h5", H5Easy::File::Overwrite);
+    H5Easy::dump(file, "/coor", coor);
+    H5Easy::dump(file, "/conn", conn);
+    H5Easy::dump(file, "/disp", disp);
+    H5Easy::dump(file, "/Sig", SigAv);
+
+    return 0;
+}
---
+++ docs/examples/statics/MixedPeriodic_LinearElastic/example.py
@@ -0,0 +1,188 @@
+import sys
+import GooseFEM
+import GMatElastic
+import numpy as np
+
+# mesh
+# ----
+
+# define mesh
+mesh = GooseFEM.Mesh.Quad4.Regular(5, 5)
+
+# mesh dimensions
+nelem = mesh.nelem()
+nne = mesh.nne()
+ndim = mesh.ndim()
+
+# mesh definitions
+coor = mesh.coor()
+conn = mesh.conn()
+dofs = mesh.dofs()
+
+# node sets
+nodesLft = mesh.nodesLeftOpenEdge()
+nodesRgt = mesh.nodesRightOpenEdge()
+nodesTop = mesh.nodesTopEdge()
+nodesBot = mesh.nodesBottomEdge()
+
+# periodicity and fixed displacements DOFs
+# ----------------------------------------
+
+for j in range(coor.shape[1]):
+    dofs[nodesRgt, j] = dofs[nodesLft, j]
+
+dofs = GooseFEM.Mesh.renumber(dofs)
+
+iip = np.concatenate((
+    dofs[nodesBot, 0],
+    dofs[nodesBot, 1],
+    dofs[nodesTop, 0],
+    dofs[nodesTop, 1]
+))
+
+# simulation variables
+# --------------------
+
+# vector definition
+vector = GooseFEM.VectorPartitioned(conn, dofs, iip)
+
+# allocate system matrix
+K = GooseFEM.MatrixPartitioned(conn, dofs, iip)
+
+# nodal quantities
+disp = np.zeros(coor.shape)
+fint = np.zeros(coor.shape)
+fext = np.zeros(coor.shape)
+fres = np.zeros(coor.shape)
+
+# element vectors
+ue = np.empty((nelem, nne, ndim))
+fe = np.empty((nelem, nne, ndim))
+Ke = np.empty((nelem, nne * ndim, nne * ndim))
+
+# element/material definition
+# ---------------------------
+
+# element definition
+elem = GooseFEM.Element.Quad4.QuadraturePlanar(vector.AsElement(coor))
+nip = elem.nip()
+
+# material definition
+mat = GMatElastic.Cartesian3d.Matrix(nelem, nip)
+Ihard = np.zeros((nelem, nip), dtype='int')
+Ihard[[0, 1, 5, 6], :] = 1
+Isoft = np.ones((nelem, nip), dtype='int') - Ihard
+mat.setElastic(Isoft, 10.0, 1.0)
+mat.setElastic(Ihard, 10.0, 10.0)
+
+# integration point tensors
+Eps = np.empty((nelem, nip, 3, 3))
+Sig = np.empty((nelem, nip, 3, 3))
+C = np.empty((nelem, nip, 3, 3, 3, 3))
+
+# solve
+# -----
+
+# strain
+ue = vector.AsElement(disp)
+Eps = elem.SymGradN_vector(ue)
+
+# stress & tangent
+(Sig, C) = mat.Tangent(Eps)
+
+# internal force
+fe = elem.Int_gradN_dot_tensor2_dV(Sig)
+fint = vector.AssembleNode(fe)
+
+# stiffness matrix
+Ke = elem.Int_gradN_dot_tensor4_dot_gradNT_dV(C)
+K.assemble(Ke)
+
+# set fixed displacements
+disp[nodesTop, 0] = +0.1
+
+# residual
+fres = fext - fint
+
+# solve
+disp = K.Solve(fres, disp)
+
+# post-process
+# ------------
+
+# compute strain and stress
+ue = vector.AsElement(disp)
+Eps = elem.SymGradN_vector(ue)
+Sig = mat.Stress(Eps)
+
+# internal force
+fe = elem.Int_gradN_dot_tensor2_dV(Sig)
+fint = vector.AssembleNode(fe)
+
+# apply reaction force
+fext = vector.Copy_p(fint, fext)
+
+# residual
+fres = fext - fint
+
+# print residual
+print(np.sum(np.abs(fres)) / np.sum(np.abs(fext)))
+
+# average stress per node
+dV = elem.DV(2)
+Sig = np.average(Sig, weights=dV, axis=1)
+
+# skip plot with "--no-plot" command line argument
+# ------------------------------------------------
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == "--no-plot":
+        sys.exit(0)
+
+# plot
+# ----
+
+import matplotlib.pyplot as plt
+import GooseMPL as gplt
+
+plt.style.use(['goose', 'goose-latex'])
+
+# extract dimension
+
+nelem = conn.shape[0]
+
+# tensor products
+
+def ddot22(A2, B2):
+    return np.einsum('eij, eji -> e', A2, B2)
+
+
+def ddot42(A4, B2):
+    return np.einsum('eijkl, elk -> eij', A4, B2)
+
+
+def dyad22(A2, B2):
+    return np.einsum('eij, ekl -> eijkl', A2, B2)
+
+# identity tensors
+
+i = np.eye(3)
+I = np.einsum('ij, e', i, np.ones([nelem]))
+I4 = np.einsum('ijkl, e -> eijkl', np.einsum('il, jk', i, i), np.ones([nelem]))
+I4rt = np.einsum('ijkl, e -> eijkl', np.einsum('ik,jl', i, i), np.ones([nelem]))
+I4s = 0.5 * (I4 + I4rt)
+II = dyad22(I, I)
+I4d = I4s - II / 3.0
+
+# compute equivalent stress
+
+Sigd = ddot42(I4d, Sig)
+sigeq = np.sqrt(3.0 / 2.0 * ddot22(Sigd, Sigd))
+
+# plot
+
+fig, ax = plt.subplots()
+gplt.patch(coor=coor + disp, conn=conn, cindex=sigeq, cmap='jet', axis=ax, clim=(0, 0.1))
+gplt.patch(coor=coor, conn=conn, linestyle='--', axis=ax)
+plt.savefig('plot.pdf')
+plt.close()
