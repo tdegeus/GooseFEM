f7c2c3c6b90ac401744a4cafb9fe29e4bf7d51ca
Tom de Geus <tom@geus.me>
2020-03-05 10:24:33 +0100

    Formatting: docs/examples/statics/FixedDisplacements_LinearElastic

--- docs/examples/statics/FixedDisplacements_LinearElastic/manual_partition.py
+++ docs/examples/statics/FixedDisplacements_LinearElastic/manual_partition.py
@@ -1,3 +1,4 @@
+import sys
 import GooseFEM
 import GMatElastic
 import numpy as np
@@ -6,12 +7,12 @@ import numpy as np
 # ----
 
 # define mesh
-mesh = GooseFEM.Mesh.Quad4.Regular(5,5)
+mesh = GooseFEM.Mesh.Quad4.Regular(5, 5)
 
 # mesh dimensions
 nelem = mesh.nelem()
-nne   = mesh.nne()
-ndim  = mesh.ndim()
+nne = mesh.nne()
+ndim = mesh.ndim()
 
 # mesh definitions
 coor = mesh.coor()
@@ -19,19 +20,19 @@ conn = mesh.conn()
 dofs = mesh.dofs()
 
 # node sets
-nodesLeft   = mesh.nodesLeftEdge()
-nodesRight  = mesh.nodesRightEdge()
-nodesTop    = mesh.nodesTopEdge()
-nodesBottom = mesh.nodesBottomEdge()
+nodesLft = mesh.nodesLeftEdge()
+nodesRgt = mesh.nodesRightEdge()
+nodesTop = mesh.nodesTopEdge()
+nodesBot = mesh.nodesBottomEdge()
 
 # fixed displacements DOFs
 # ------------------------
 
 iip = np.concatenate((
-    dofs[nodesRight , 0],
-    dofs[nodesTop   , 1],
-    dofs[nodesLeft  , 0],
-    dofs[nodesBottom, 1]
+    dofs[nodesRgt, 0],
+    dofs[nodesTop, 1],
+    dofs[nodesLft, 0],
+    dofs[nodesBot, 1]
 ))
 
 # simulation variables
@@ -57,7 +58,7 @@ fext_p = np.zeros(vector.nnp())
 # element vectors
 ue = np.empty((nelem, nne, ndim))
 fe = np.empty((nelem, nne, ndim))
-Ke = np.empty((nelem, nne*ndim, nne*ndim))
+Ke = np.empty((nelem, nne * ndim, nne * ndim))
 
 # element/material definition
 # ---------------------------
@@ -67,13 +68,13 @@ elem = GooseFEM.Element.Quad4.QuadraturePlanar(vector.AsElement(coor))
 nip = elem.nip()
 
 # material definition
-mat = GMatElastic.Cartesian3d.Matrix(nelem, nip, 1., 1.)
+mat = GMatElastic.Cartesian3d.Matrix(nelem, nip, 1.0, 1.0)
 
 # integration point tensors
 d = 3
-Eps = np.empty((nelem, nip, d, d      ))
-Sig = np.empty((nelem, nip, d, d      ))
-C   = np.empty((nelem, nip, d, d, d, d))
+Eps = np.empty((nelem, nip, d, d))
+Sig = np.empty((nelem, nip, d, d))
+C = np.empty((nelem, nip, d, d, d, d))
 
 # solve
 # -----
@@ -95,10 +96,10 @@ K.assemble(Ke)
 
 # set fixed displacements
 u_p = np.concatenate((
-    +0.1 * np.ones(nodesRight .size),
-    -0.1 * np.ones(nodesTop   .size),
-     0.0 * np.ones(nodesLeft  .size),
-     0.0 * np.ones(nodesBottom.size)
+    +0.1 * np.ones(nodesRgt.size),
+    -0.1 * np.ones(nodesTop.size),
+    np.zeros(nodesLft.size),
+    np.zeros(nodesBot.size)
 ))
 
 # residual
@@ -132,7 +133,7 @@ fext_p = vector.AsDofs_p(fint)
 fres = fext - fint
 
 # partition
-fres_u = vector.AsDofs_u(fres);
+fres_u = vector.AsDofs_u(fres)
 
 # print residual
 print(np.sum(np.abs(fres_u)) / np.sum(np.abs(fext_p)))
@@ -144,8 +145,6 @@ Sig = np.average(Sig, weights=dV, axis=1)
 # skip plot with "--no-plot" command line argument
 # ------------------------------------------------
 
-import sys
-
 if len(sys.argv) == 2:
     if sys.argv[1] == "--no-plot":
         sys.exit(0)
@@ -153,37 +152,47 @@ if len(sys.argv) == 2:
 # plot
 # ----
 
-import matplotlib.pyplot as plt
 import GooseMPL as gplt
+import matplotlib.pyplot as plt
 
 plt.style.use(['goose', 'goose-latex'])
 
 # extract dimension
+
 nelem = conn.shape[0]
 
 # tensor products
-ddot22 = lambda A2,B2: np.einsum('eij  ,eji->e    ',A2,B2)
-ddot42 = lambda A4,B2: np.einsum('eijkl,elk->eij  ',A4,B2)
-dyad22 = lambda A2,B2: np.einsum('eij  ,ekl->eijkl',A2,B2)
 
-# identity tensor (single tensor)
-i    = np.eye(3)
+def ddot22(A2, B2):
+    return np.einsum('eij, eji -> e', A2, B2)
 
-# identity tensors (grid)
-I    = np.einsum('ij  ,e'       ,                  i   ,np.ones([nelem]))
-I4   = np.einsum('ijkl,e->eijkl',np.einsum('il,jk',i,i),np.ones([nelem]))
-I4rt = np.einsum('ijkl,e->eijkl',np.einsum('ik,jl',i,i),np.ones([nelem]))
-I4s  = (I4+I4rt)/2.
-II   = dyad22(I,I)
-I4d  = I4s-II/3.
+
+def ddot42(A4, B2):
+    return np.einsum('eijkl, elk -> eij', A4, B2)
+
+
+def dyad22(A2, B2):
+    return np.einsum('eij, ekl -> eijkl', A2, B2)
+
+# identity tensors
+
+i = np.eye(3)
+I = np.einsum('ij, e', i, np.ones([nelem]))
+I4 = np.einsum('ijkl, e -> eijkl', np.einsum('il, jk', i, i), np.ones([nelem]))
+I4rt = np.einsum('ijkl, e -> eijkl', np.einsum('ik,jl', i, i), np.ones([nelem]))
+I4s = 0.5 * (I4 + I4rt)
+II = dyad22(I, I)
+I4d = I4s - II / 3.0
 
 # compute equivalent stress
-Sigd  = ddot42(I4d, Sig)
-sigeq = np.sqrt(3./2.*ddot22(Sigd,Sigd))
+
+Sigd = ddot42(I4d, Sig)
+sigeq = np.sqrt(3.0 / 2.0 * ddot22(Sigd, Sigd))
 
 # plot
+
 fig, ax = plt.subplots()
-gplt.patch(coor=coor+disp, conn=conn, cindex=sigeq, cmap='jet', axis=ax, clim=(0,0.1))
+gplt.patch(coor=coor + disp, conn=conn, cindex=sigeq, cmap='jet', axis=ax, clim=(0, 0.1))
 gplt.patch(coor=coor, conn=conn, linestyle='--', axis=ax)
 plt.savefig('plot.pdf')
 plt.close()
