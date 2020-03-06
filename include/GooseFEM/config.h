f5d9138c950750992f97c1ecf2bcc5d3a2cd4e04
Tom de Geus <tom@geus.me>
2020-03-06 09:59:01 +0100

    Applying style to headers

--- include/GooseFEM/config.h
+++ include/GooseFEM/config.h
@@ -1,28 +1,11 @@
-/* =================================================================================================
+/*
 
 (c - GPLv3) T.W.J. de Geus (Tom) | tom@geus.me | www.geus.me | github.com/tdegeus/GooseFEM
 
-================================================================================================= */
+*/
 
 #ifndef GOOSEFEM_CONFIG_H
 #define GOOSEFEM_CONFIG_H
-
-// -------------------------------------------------------------------------------------------------
-
-#ifndef GOOSEFEM_DEBUG
-    #ifndef NDEBUG
-        #define NDEBUG
-    #endif
-#else
-    #ifndef GOOSEFEM_ENABLE_ASSERT
-        #define GOOSEFEM_ENABLE_ASSERT
-    #endif
-    #ifndef XTENSOR_ENABLE_ASSERT
-        #define XTENSOR_ENABLE_ASSERT
-    #endif
-#endif
-
-// -------------------------------------------------------------------------------------------------
 
 #define _USE_MATH_DEFINES // to use "M_PI" from "math.h"
 
@@ -54,59 +37,40 @@
 
 using namespace xt::placeholders;
 
-// -------------------------------------------------------------------------------------------------
-
-// dummy operation that can be use to suppress the "unused parameter" warnings
-#define UNUSED(p) ( (void)(p) )
-
-// -------------------------------------------------------------------------------------------------
-
-#ifndef NDEBUG
-#ifndef GOOSEFEM_ENABLE_ASSERT
-#define GOOSEFEM_ENABLE_ASSERT
-#endif
-#endif
+#define UNUSED(p) ((void)(p))
 
 #ifdef GOOSEFEM_ENABLE_ASSERT
-#define GOOSEFEM_ASSERT(expr) GOOSEFEM_ASSERT_IMPL(expr, __FILE__, __LINE__)
-#define GOOSEFEM_ASSERT_IMPL(expr, file, line) \
-    if (!(expr)) \
-    { \
-        throw std::runtime_error( \
-            std::string(file) + ':' + std::to_string(line) + \
-            ": assertion failed (" #expr ") \n\t"); \
-    }
+    #define GOOSEFEM_ASSERT(expr) GOOSEFEM_ASSERT_IMPL(expr, __FILE__, __LINE__)
+    #define GOOSEFEM_ASSERT_IMPL(expr, file, line) \
+        if (!(expr)) { \
+            throw std::runtime_error( \
+                std::string(file) + ':' + std::to_string(line) + \
+                ": assertion failed (" #expr ") \n\t"); \
+        }
 #else
-#define GOOSEFEM_ASSERT(expr)
+    #define GOOSEFEM_ASSERT(expr)
 #endif
-
-// -------------------------------------------------------------------------------------------------
 
 #define GOOSEFEM_CHECK(expr) GOOSEFEM_CHECK_IMPL(expr, __FILE__, __LINE__)
 #define GOOSEFEM_CHECK_IMPL(expr, file, line) \
-    if (!(expr)) \
-    { \
+    if (!(expr)) { \
         throw std::runtime_error( \
             std::string(file) + ':' + std::to_string(line) + \
             ": assertion failed (" #expr ") \n\t"); \
     }
-
-// -------------------------------------------------------------------------------------------------
 
 #define GOOSEFEM_VERSION_MAJOR 0
 #define GOOSEFEM_VERSION_MINOR 3
 #define GOOSEFEM_VERSION_PATCH 0
 
-#define GOOSEFEM_VERSION_AT_LEAST(x,y,z) \
+#define GOOSEFEM_VERSION_AT_LEAST(x, y, z) \
     (GOOSEFEM_VERSION_MAJOR > x || (GOOSEFEM_VERSION_MAJOR >= x && \
     (GOOSEFEM_VERSION_MINOR > y || (GOOSEFEM_VERSION_MINOR >= y && \
                                     GOOSEFEM_VERSION_PATCH >= z))))
 
-#define GOOSEFEM_VERSION(x,y,z) \
+#define GOOSEFEM_VERSION(x, y, z) \
     (GOOSEFEM_VERSION_MAJOR == x && \
      GOOSEFEM_VERSION_MINOR == y && \
      GOOSEFEM_VERSION_PATCH == z)
-
-// -------------------------------------------------------------------------------------------------
 
 #endif
