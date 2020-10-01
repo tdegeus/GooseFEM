088b2ebfa0161775557afd054780d5365249a34d
Tom de Geus <tom@geus.me>
2020-10-01 14:23:30 +0200

    Formatting update

--- test/MatrixDiagonal.cpp
+++ test/MatrixDiagonal.cpp
@@ -1,78 +1,40 @@
 
 #include "support.h"
 
-// =================================================================================================
-
 TEST_CASE("GooseFEM::MatrixDiagonal", "MatrixDiagonal.h")
 {
 
-// =================================================================================================
-
 SECTION("dot")
 {
-  // mesh
-  GooseFEM::Mesh::Quad4::Regular mesh(2,2);
+    GooseFEM::Mesh::Quad4::Regular mesh(2, 2);
 
-  // random matrix and column
-  xt::xtensor<double,1> a = xt::random::rand<double>({mesh.nnode()*mesh.ndim()});
-  xt::xtensor<double,1> b = xt::random::rand<double>({mesh.nnode()*mesh.ndim()});
-  xt::xtensor<double,1> c;
+    xt::xtensor<double, 1> a = xt::random::rand<double>({mesh.nnode() * mesh.ndim()});
+    xt::xtensor<double, 1> b = xt::random::rand<double>({mesh.nnode() * mesh.ndim()});
+    xt::xtensor<double, 1> c = a * b;
 
-  // compute product
-  c = a * b;
+    GooseFEM::MatrixDiagonal A(mesh.conn(), mesh.dofs());
+    A.set(a);
+    xt::xtensor<double, 1> C = A.Dot(b);
 
-  // convert to GooseFEM
-  // - allocate
-  GooseFEM::MatrixDiagonal A(mesh.conn(), mesh.dofs());
-  xt::xtensor<double,1> C;
-  // - set
-  A.set(a);
-
-  // compute product
-  C = A.Dot(b);
-
-  // check
-  // - size
-  REQUIRE( C.size() == c.size() );
-  // - components
-  REQUIRE( xt::allclose(C, c) );
+    REQUIRE(C.size() == c.size());
+    REQUIRE(xt::allclose(C, c));
 }
-
-// -------------------------------------------------------------------------------------------------
 
 SECTION("solve")
 {
-  // mesh
-  GooseFEM::Mesh::Quad4::Regular mesh(2,2);
+    GooseFEM::Mesh::Quad4::Regular mesh(2, 2);
 
-  // random matrix and column
-  xt::xtensor<double,1> a = xt::random::rand<double>({mesh.nnode()*mesh.ndim()});
-  xt::xtensor<double,1> b = xt::random::rand<double>({mesh.nnode()*mesh.ndim()});
-  xt::xtensor<double,1> c;
+    xt::xtensor<double, 1> a = xt::random::rand<double>({mesh.nnode() * mesh.ndim()});
+    xt::xtensor<double, 1> b = xt::random::rand<double>({mesh.nnode() * mesh.ndim()});
+    xt::xtensor<double, 1> c = a * b;
 
-  // compute product
-  c = a * b;
+    GooseFEM::MatrixDiagonal A(mesh.conn(), mesh.dofs());
+    A.set(a);
+    xt::xtensor<double, 1> C = A.Dot(b);
+    xt::xtensor<double, 1> B = A.Solve(C);
 
-  // convert to GooseFEM
-  // - allocate
-  GooseFEM::MatrixDiagonal A(mesh.conn(), mesh.dofs());
-  xt::xtensor<double,1> B, C;
-  // - set
-  A.set(a);
-
-  // compute product
-  C = A.Dot(b);
-
-  // solve
-  B = A.Solve(C);
-
-  // check
-  // - size
-  REQUIRE( B.size() == b.size() );
-  // - components
-  REQUIRE( xt::allclose(B, b) );
+    REQUIRE(B.size() == b.size());
+    REQUIRE(xt::allclose(B, b));
 }
-
-// // =================================================================================================
 
 }
