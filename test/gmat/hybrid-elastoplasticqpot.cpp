2aadff920c182a68871ac5e01865595ba2002315
Tom de Geus <tom@geus.me>
2020-10-06 11:39:47 +0200

    Adding test GMatElastoPlasticQPot

--- test/gmat/hybrid-elastoplasticqpot.cpp
+++ test/gmat/hybrid-elastoplasticqpot.cpp
@@ -4,14 +4,14 @@
 #include <xtensor/xmath.hpp>
 #include <Eigen/Eigen>
 #include <GooseFEM/GooseFEM.h>
-#include <GMatElastic/Cartesian3d.h>
+#include <GMatElastoPlasticQPot/Cartesian2d.h>
 
 #define ISCLOSE(a,b) REQUIRE_THAT((a), Catch::WithinAbs((b), 1.0e-12));
 
-TEST_CASE("hybrid-elastic", "GooseFEM.h")
+TEST_CASE("hybrid-elastoplasticqpot", "GooseFEM.h")
 {
 
-    SECTION("Vector/Matrix - GMatElastic")
+    SECTION("Vector/Matrix - GMatElastoPlasticQPot")
     {
         size_t N = 5;
         GooseFEM::Mesh::Quad4::Regular mesh(N, N);
@@ -32,31 +32,46 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
         GooseFEM::Vector vector_a(conn_a, dofs);
         GooseFEM::Vector vector_b(conn_b, dofs);
 
-        GooseFEM::Matrix K(conn, dofs);
-        GooseFEM::Matrix K_a(conn_a, dofs);
         GooseFEM::Matrix K_b(conn_b, dofs);
 
         auto coor = mesh.coor();
 
-        GooseFEM::Element::Quad4::QuadraturePlanar quad(vector.AsElement(coor));
-        GooseFEM::Element::Quad4::QuadraturePlanar quad_a(vector_a.AsElement(coor));
-        GooseFEM::Element::Quad4::QuadraturePlanar quad_b(vector_b.AsElement(coor));
+        GooseFEM::Element::Quad4::Quadrature quad(vector.AsElement(coor));
+        GooseFEM::Element::Quad4::Quadrature quad_a(vector_a.AsElement(coor));
+        GooseFEM::Element::Quad4::Quadrature quad_b(vector_b.AsElement(coor));
         size_t nip = quad.nip();
 
-        GMatElastic::Cartesian3d::Matrix mat(nelem, nip);
-        GMatElastic::Cartesian3d::Matrix mat_a(nelem_a, nip, 3.0, 4.0);
-        GMatElastic::Cartesian3d::Matrix mat_b(nelem_b, nip, 5.0, 6.0);
+        GMatElastoPlasticQPot::Cartesian2d::Array<2> mat({nelem, nip});
+        GMatElastoPlasticQPot::Cartesian2d::Array<2> mat_a({nelem_a, nip});
+        GMatElastoPlasticQPot::Cartesian2d::Array<2> mat_b({nelem_b, nip});
+
+        xt::xtensor<double, 2> epsy_a = 0.2 * xt::random::rand<double>(std::array<size_t, 2>{nelem_a, 100});
+        epsy_a = xt::cumsum(epsy_a, 1);
 
         {
             xt::xtensor<size_t, 2> I = xt::zeros<size_t>({nelem, nip});
+            xt::xtensor<size_t, 2> idx = xt::zeros<size_t>({nelem, nip});
             xt::view(I, xt::keep(elem_a), xt::all()) = 1;
-            mat.setElastic(I, 3.0, 4.0);
+            xt::view(idx, xt::keep(elem_a), xt::all()) = xt::arange<size_t>(nelem_a).reshape({-1, 1});
+            mat.setCusp(I, idx, 3.0 * xt::ones<double>({nelem_a}), 4.0 * xt::ones<double>({nelem_a}), epsy_a);
         }
 
         {
             xt::xtensor<size_t, 2> I = xt::zeros<size_t>({nelem, nip});
             xt::view(I, xt::keep(elem_b), xt::all()) = 1;
             mat.setElastic(I, 5.0, 6.0);
+        }
+
+        {
+            xt::xtensor<size_t, 2> I = xt::ones<size_t>({nelem_a, nip});
+            xt::xtensor<size_t, 2> idx = xt::zeros<size_t>({nelem_a, nip});
+            xt::view(idx, xt::range(0, nelem_a), xt::all()) = xt::arange<size_t>(nelem_a).reshape({-1, 1});
+            mat_a.setCusp(I, idx, 3.0 * xt::ones<double>({nelem_a}), 4.0 * xt::ones<double>({nelem_a}), epsy_a);
+        }
+
+        {
+            xt::xtensor<size_t, 2> I = xt::ones<size_t>({nelem_b, nip});
+            mat_b.setElastic(I, 5.0, 6.0);
         }
 
         mat.check();
@@ -71,19 +86,16 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
             auto Eps_a = quad_a.SymGradN_vector(vector_a.AsElement(disp));
             auto Eps_b = quad_b.SymGradN_vector(vector_b.AsElement(disp));
 
-            auto Sig = mat.Stress(Eps);
-            auto Sig_a = mat_a.Stress(Eps_a);
-            auto Sig_b = mat_b.Stress(Eps_b);
+            mat.setStrain(Eps);
+            mat_a.setStrain(Eps_a);
+            mat_b.setStrain(Eps_b);
 
-            xt::xtensor<double, 6> C;
-            xt::xtensor<double, 6> C_a;
-            xt::xtensor<double, 6> C_b;
-            std::tie(Sig, C) = mat.Tangent(Eps);
-            std::tie(Sig_a, C_a) = mat_a.Tangent(Eps_a);
-            std::tie(Sig_b, C_b) = mat_b.Tangent(Eps_b);
+            auto Sig = mat.Stress();
+            auto Sig_a = mat_a.Stress();
+            auto Sig_b = mat_b.Stress();
 
-            K.assemble(quad.Int_gradN_dot_tensor4_dot_gradNT_dV(C));
-            K_a.assemble(quad_a.Int_gradN_dot_tensor4_dot_gradNT_dV(C_a));
+            auto C_b = mat_b.Tangent();
+
             K_b.assemble(quad_b.Int_gradN_dot_tensor4_dot_gradNT_dV(C_b));
 
             auto f = vector.AssembleNode(quad.Int_gradN_dot_tensor2_dV(Sig));
@@ -91,22 +103,16 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
             auto f_b = vector_b.AssembleNode(quad_b.Int_gradN_dot_tensor2_dV(Sig_b));
 
             REQUIRE(xt::allclose(f, f_a + f_b));
-            REQUIRE(xt::allclose(f, K.Dot(disp)));
-            REQUIRE(xt::allclose(f_a, K_a.Dot(disp)));
             REQUIRE(xt::allclose(f_b, K_b.Dot(disp)));
             REQUIRE(xt::allclose(f, f_a + K_b.Dot(disp)));
-            REQUIRE(xt::allclose(f, f_b + K_a.Dot(disp)));
 
             auto fd = vector.AssembleDofs(quad.Int_gradN_dot_tensor2_dV(Sig));
             auto fd_a = vector_a.AssembleDofs(quad_a.Int_gradN_dot_tensor2_dV(Sig_a));
             auto fd_b = vector_b.AssembleDofs(quad_b.Int_gradN_dot_tensor2_dV(Sig_b));
 
             REQUIRE(xt::allclose(fd, fd_a + fd_b));
-            REQUIRE(xt::allclose(fd, K.Dot(vector.AsDofs(disp))));
-            REQUIRE(xt::allclose(fd_a, K_a.Dot(vector.AsDofs(disp))));
             REQUIRE(xt::allclose(fd_b, K_b.Dot(vector.AsDofs(disp))));
             REQUIRE(xt::allclose(fd, fd_a + K_b.Dot(vector.AsDofs(disp))));
-            REQUIRE(xt::allclose(fd, fd_b + K_a.Dot(vector.AsDofs(disp))));
 
             auto Sig_c = decltype(Sig)::from_shape(Sig.shape());
             xt::view(Sig_c, xt::keep(elem_a)) = Sig_a;
@@ -116,7 +122,7 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
         }
     }
 
-    SECTION("Vector/Matrix - GMatElastic - Periodic")
+    SECTION("Vector/Matrix - GMatElastoPlasticQPot - Periodic")
     {
         size_t N = 5;
         GooseFEM::Mesh::Quad4::Regular mesh(N, N);
@@ -137,31 +143,46 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
         GooseFEM::Vector vector_a(conn_a, dofs);
         GooseFEM::Vector vector_b(conn_b, dofs);
 
-        GooseFEM::Matrix K(conn, dofs);
-        GooseFEM::Matrix K_a(conn_a, dofs);
         GooseFEM::Matrix K_b(conn_b, dofs);
 
         auto coor = mesh.coor();
 
-        GooseFEM::Element::Quad4::QuadraturePlanar quad(vector.AsElement(coor));
-        GooseFEM::Element::Quad4::QuadraturePlanar quad_a(vector_a.AsElement(coor));
-        GooseFEM::Element::Quad4::QuadraturePlanar quad_b(vector_b.AsElement(coor));
+        GooseFEM::Element::Quad4::Quadrature quad(vector.AsElement(coor));
+        GooseFEM::Element::Quad4::Quadrature quad_a(vector_a.AsElement(coor));
+        GooseFEM::Element::Quad4::Quadrature quad_b(vector_b.AsElement(coor));
         size_t nip = quad.nip();
 
-        GMatElastic::Cartesian3d::Matrix mat(nelem, nip);
-        GMatElastic::Cartesian3d::Matrix mat_a(nelem_a, nip, 3.0, 4.0);
-        GMatElastic::Cartesian3d::Matrix mat_b(nelem_b, nip, 5.0, 6.0);
+        GMatElastoPlasticQPot::Cartesian2d::Array<2> mat({nelem, nip});
+        GMatElastoPlasticQPot::Cartesian2d::Array<2> mat_a({nelem_a, nip});
+        GMatElastoPlasticQPot::Cartesian2d::Array<2> mat_b({nelem_b, nip});
+
+        xt::xtensor<double, 2> epsy_a = 0.2 * xt::random::rand<double>(std::array<size_t, 2>{nelem_a, 100});
+        epsy_a = xt::cumsum(epsy_a, 1);
 
         {
             xt::xtensor<size_t, 2> I = xt::zeros<size_t>({nelem, nip});
+            xt::xtensor<size_t, 2> idx = xt::zeros<size_t>({nelem, nip});
             xt::view(I, xt::keep(elem_a), xt::all()) = 1;
-            mat.setElastic(I, 3.0, 4.0);
+            xt::view(idx, xt::keep(elem_a), xt::all()) = xt::arange<size_t>(nelem_a).reshape({-1, 1});
+            mat.setCusp(I, idx, 3.0 * xt::ones<double>({nelem_a}), 4.0 * xt::ones<double>({nelem_a}), epsy_a);
         }
 
         {
             xt::xtensor<size_t, 2> I = xt::zeros<size_t>({nelem, nip});
             xt::view(I, xt::keep(elem_b), xt::all()) = 1;
             mat.setElastic(I, 5.0, 6.0);
+        }
+
+        {
+            xt::xtensor<size_t, 2> I = xt::ones<size_t>({nelem_a, nip});
+            xt::xtensor<size_t, 2> idx = xt::zeros<size_t>({nelem_a, nip});
+            xt::view(idx, xt::range(0, nelem_a), xt::all()) = xt::arange<size_t>(nelem_a).reshape({-1, 1});
+            mat_a.setCusp(I, idx, 3.0 * xt::ones<double>({nelem_a}), 4.0 * xt::ones<double>({nelem_a}), epsy_a);
+        }
+
+        {
+            xt::xtensor<size_t, 2> I = xt::ones<size_t>({nelem_b, nip});
+            mat_b.setElastic(I, 5.0, 6.0);
         }
 
         mat.check();
@@ -177,19 +198,16 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
             auto Eps_a = quad_a.SymGradN_vector(vector_a.AsElement(disp));
             auto Eps_b = quad_b.SymGradN_vector(vector_b.AsElement(disp));
 
-            auto Sig = mat.Stress(Eps);
-            auto Sig_a = mat_a.Stress(Eps_a);
-            auto Sig_b = mat_b.Stress(Eps_b);
+            mat.setStrain(Eps);
+            mat_a.setStrain(Eps_a);
+            mat_b.setStrain(Eps_b);
 
-            xt::xtensor<double, 6> C;
-            xt::xtensor<double, 6> C_a;
-            xt::xtensor<double, 6> C_b;
-            std::tie(Sig, C) = mat.Tangent(Eps);
-            std::tie(Sig_a, C_a) = mat_a.Tangent(Eps_a);
-            std::tie(Sig_b, C_b) = mat_b.Tangent(Eps_b);
+            auto Sig = mat.Stress();
+            auto Sig_a = mat_a.Stress();
+            auto Sig_b = mat_b.Stress();
 
-            K.assemble(quad.Int_gradN_dot_tensor4_dot_gradNT_dV(C));
-            K_a.assemble(quad_a.Int_gradN_dot_tensor4_dot_gradNT_dV(C_a));
+            auto C_b = mat_b.Tangent();
+
             K_b.assemble(quad_b.Int_gradN_dot_tensor4_dot_gradNT_dV(C_b));
 
             auto f = vector.AssembleNode(quad.Int_gradN_dot_tensor2_dV(Sig));
@@ -197,22 +215,16 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
             auto f_b = vector_b.AssembleNode(quad_b.Int_gradN_dot_tensor2_dV(Sig_b));
 
             REQUIRE(xt::allclose(f, f_a + f_b));
-            REQUIRE(xt::allclose(f, K.Dot(disp)));
-            REQUIRE(xt::allclose(f_a, K_a.Dot(disp)));
             REQUIRE(xt::allclose(f_b, K_b.Dot(disp)));
             REQUIRE(xt::allclose(f, f_a + K_b.Dot(disp)));
-            REQUIRE(xt::allclose(f, f_b + K_a.Dot(disp)));
 
             auto fd = vector.AssembleDofs(quad.Int_gradN_dot_tensor2_dV(Sig));
             auto fd_a = vector_a.AssembleDofs(quad_a.Int_gradN_dot_tensor2_dV(Sig_a));
             auto fd_b = vector_b.AssembleDofs(quad_b.Int_gradN_dot_tensor2_dV(Sig_b));
 
             REQUIRE(xt::allclose(fd, fd_a + fd_b));
-            REQUIRE(xt::allclose(fd, K.Dot(vector.AsDofs(disp))));
-            REQUIRE(xt::allclose(fd_a, K_a.Dot(vector.AsDofs(disp))));
             REQUIRE(xt::allclose(fd_b, K_b.Dot(vector.AsDofs(disp))));
             REQUIRE(xt::allclose(fd, fd_a + K_b.Dot(vector.AsDofs(disp))));
-            REQUIRE(xt::allclose(fd, fd_b + K_a.Dot(vector.AsDofs(disp))));
 
             auto Sig_c = decltype(Sig)::from_shape(Sig.shape());
             xt::view(Sig_c, xt::keep(elem_a)) = Sig_a;
@@ -222,7 +234,7 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
         }
     }
 
-    SECTION("VectorPartitioned/Matrix - GMatElastic - Semi-Periodic")
+    SECTION("VectorPartitioned/Matrix - GMatElastoPlasticQPot - Semi-Periodic")
     {
         size_t N = 5;
         GooseFEM::Mesh::Quad4::Regular mesh(N, N);
@@ -258,31 +270,46 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
         GooseFEM::VectorPartitioned vector_a(conn_a, dofs, iip);
         GooseFEM::VectorPartitioned vector_b(conn_b, dofs, iip);
 
-        GooseFEM::Matrix K(conn, dofs);
-        GooseFEM::Matrix K_a(conn_a, dofs);
         GooseFEM::Matrix K_b(conn_b, dofs);
 
         auto coor = mesh.coor();
 
-        GooseFEM::Element::Quad4::QuadraturePlanar quad(vector.AsElement(coor));
-        GooseFEM::Element::Quad4::QuadraturePlanar quad_a(vector_a.AsElement(coor));
-        GooseFEM::Element::Quad4::QuadraturePlanar quad_b(vector_b.AsElement(coor));
+        GooseFEM::Element::Quad4::Quadrature quad(vector.AsElement(coor));
+        GooseFEM::Element::Quad4::Quadrature quad_a(vector_a.AsElement(coor));
+        GooseFEM::Element::Quad4::Quadrature quad_b(vector_b.AsElement(coor));
         size_t nip = quad.nip();
 
-        GMatElastic::Cartesian3d::Matrix mat(nelem, nip);
-        GMatElastic::Cartesian3d::Matrix mat_a(nelem_a, nip, 3.0, 4.0);
-        GMatElastic::Cartesian3d::Matrix mat_b(nelem_b, nip, 5.0, 6.0);
+        GMatElastoPlasticQPot::Cartesian2d::Array<2> mat({nelem, nip});
+        GMatElastoPlasticQPot::Cartesian2d::Array<2> mat_a({nelem_a, nip});
+        GMatElastoPlasticQPot::Cartesian2d::Array<2> mat_b({nelem_b, nip});
+
+        xt::xtensor<double, 2> epsy_a = 0.2 * xt::random::rand<double>(std::array<size_t, 2>{nelem_a, 100});
+        epsy_a = xt::cumsum(epsy_a, 1);
 
         {
             xt::xtensor<size_t, 2> I = xt::zeros<size_t>({nelem, nip});
+            xt::xtensor<size_t, 2> idx = xt::zeros<size_t>({nelem, nip});
             xt::view(I, xt::keep(elem_a), xt::all()) = 1;
-            mat.setElastic(I, 3.0, 4.0);
+            xt::view(idx, xt::keep(elem_a), xt::all()) = xt::arange<size_t>(nelem_a).reshape({-1, 1});
+            mat.setCusp(I, idx, 3.0 * xt::ones<double>({nelem_a}), 4.0 * xt::ones<double>({nelem_a}), epsy_a);
         }
 
         {
             xt::xtensor<size_t, 2> I = xt::zeros<size_t>({nelem, nip});
             xt::view(I, xt::keep(elem_b), xt::all()) = 1;
             mat.setElastic(I, 5.0, 6.0);
+        }
+
+        {
+            xt::xtensor<size_t, 2> I = xt::ones<size_t>({nelem_a, nip});
+            xt::xtensor<size_t, 2> idx = xt::zeros<size_t>({nelem_a, nip});
+            xt::view(idx, xt::range(0, nelem_a), xt::all()) = xt::arange<size_t>(nelem_a).reshape({-1, 1});
+            mat_a.setCusp(I, idx, 3.0 * xt::ones<double>({nelem_a}), 4.0 * xt::ones<double>({nelem_a}), epsy_a);
+        }
+
+        {
+            xt::xtensor<size_t, 2> I = xt::ones<size_t>({nelem_b, nip});
+            mat_b.setElastic(I, 5.0, 6.0);
         }
 
         mat.check();
@@ -298,19 +325,16 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
             auto Eps_a = quad_a.SymGradN_vector(vector_a.AsElement(disp));
             auto Eps_b = quad_b.SymGradN_vector(vector_b.AsElement(disp));
 
-            auto Sig = mat.Stress(Eps);
-            auto Sig_a = mat_a.Stress(Eps_a);
-            auto Sig_b = mat_b.Stress(Eps_b);
+            mat.setStrain(Eps);
+            mat_a.setStrain(Eps_a);
+            mat_b.setStrain(Eps_b);
 
-            xt::xtensor<double, 6> C;
-            xt::xtensor<double, 6> C_a;
-            xt::xtensor<double, 6> C_b;
-            std::tie(Sig, C) = mat.Tangent(Eps);
-            std::tie(Sig_a, C_a) = mat_a.Tangent(Eps_a);
-            std::tie(Sig_b, C_b) = mat_b.Tangent(Eps_b);
+            auto Sig = mat.Stress();
+            auto Sig_a = mat_a.Stress();
+            auto Sig_b = mat_b.Stress();
 
-            K.assemble(quad.Int_gradN_dot_tensor4_dot_gradNT_dV(C));
-            K_a.assemble(quad_a.Int_gradN_dot_tensor4_dot_gradNT_dV(C_a));
+            auto C_b = mat_b.Tangent();
+
             K_b.assemble(quad_b.Int_gradN_dot_tensor4_dot_gradNT_dV(C_b));
 
             auto f = vector.AssembleNode(quad.Int_gradN_dot_tensor2_dV(Sig));
@@ -318,22 +342,16 @@ TEST_CASE("hybrid-elastic", "GooseFEM.h")
             auto f_b = vector_b.AssembleNode(quad_b.Int_gradN_dot_tensor2_dV(Sig_b));
 
             REQUIRE(xt::allclose(f, f_a + f_b));
-            REQUIRE(xt::allclose(f, K.Dot(disp)));
-            REQUIRE(xt::allclose(f_a, K_a.Dot(disp)));
             REQUIRE(xt::allclose(f_b, K_b.Dot(disp)));
             REQUIRE(xt::allclose(f, f_a + K_b.Dot(disp)));
-            REQUIRE(xt::allclose(f, f_b + K_a.Dot(disp)));
 
             auto fd = vector.AssembleDofs(quad.Int_gradN_dot_tensor2_dV(Sig));
             auto fd_a = vector_a.AssembleDofs(quad_a.Int_gradN_dot_tensor2_dV(Sig_a));
             auto fd_b = vector_b.AssembleDofs(quad_b.Int_gradN_dot_tensor2_dV(Sig_b));
 
             REQUIRE(xt::allclose(fd, fd_a + fd_b));
-            REQUIRE(xt::allclose(fd, K.Dot(vector.AsDofs(disp))));
-            REQUIRE(xt::allclose(fd_a, K_a.Dot(vector.AsDofs(disp))));
             REQUIRE(xt::allclose(fd_b, K_b.Dot(vector.AsDofs(disp))));
             REQUIRE(xt::allclose(fd, fd_a + K_b.Dot(vector.AsDofs(disp))));
-            REQUIRE(xt::allclose(fd, fd_b + K_a.Dot(vector.AsDofs(disp))));
 
             auto Sig_c = decltype(Sig)::from_shape(Sig.shape());
             xt::view(Sig_c, xt::keep(elem_a)) = Sig_a;
