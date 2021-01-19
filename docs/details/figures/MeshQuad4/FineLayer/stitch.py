9d7f5086cc00a98672330bff11f0fb17cb06dbe1
Tom de Geus <tom@geus.me>
2021-01-19 16:54:22 +0100

    Stitch: Adding nodesets to example

--- docs/details/figures/MeshQuad4/FineLayer/stitch.py
+++ docs/details/figures/MeshQuad4/FineLayer/stitch.py
@@ -32,7 +32,18 @@ cindex[stitch.elemset(np.arange(mesh.nelem()), 0)] = 1
 cindex[stitch.elemset(np.arange(mesh.nelem()), 1)] = 2
 cindex[stitch.elemset(np.arange(mesh.nelem()), 2)] = 3
 
+left = stitch.nodeset([mesh.nodesLeftEdge(), mesh.nodesLeftEdge(), mesh.nodesLeftEdge()])
+right = stitch.nodeset([mesh.nodesRightEdge(), mesh.nodesRightEdge(), mesh.nodesRightEdge()])
+top = stitch.nodeset(mesh.nodesTopEdge(), 2)
+bottom = stitch.nodeset(mesh.nodesBottomEdge(), 0)
+left = np.setdiff1d(np.setdiff1d(left, top), bottom)
+right = np.setdiff1d(np.setdiff1d(right, top), bottom)
+
 im = gplt.patch(coor=coor, conn=conn, cindex=cindex, cmap='jet', axis=ax)
+ax.plot(coor[left, 0], coor[left, 1], marker='o', c='b', ls='none')
+ax.plot(coor[right, 0], coor[right, 1], marker='o', c='r', ls='none')
+ax.plot(coor[bottom, 0], coor[bottom, 1], marker='o', c='g', ls='none')
+ax.plot(coor[top, 0], coor[top, 1], marker='o', c='c', ls='none')
 
 ax.set_aspect('equal')
 ax.get_xaxis().set_visible(False)
