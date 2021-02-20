881c600e298d223cb0d6e09aaa7e5acdf5ae078c
Tom de Geus <tom@geus.me>
2021-02-20 17:28:33 +0100

    Fixing tests

--- test/basic/version.cpp
+++ test/basic/version.cpp
@@ -9,7 +9,12 @@ TEST_CASE("GooseFEM::version", "version.h")
     SECTION("basic")
     {
         std::cout << GooseFEM::version() << std::endl;
-        std::cout << GooseFEM::version_dependencies() << std::endl;
+
+        auto deps = GooseFEM::version_dependencies();
+
+        for (auto& i : deps) {
+            std::cout << i << std::endl;
+        }
     }
 
 }
