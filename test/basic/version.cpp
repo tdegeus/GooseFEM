1185f51c7f0a0f7229267a7bf8f7de05c54fc0ac
Tom de Geus <tdegeus@users.noreply.github.com>
2021-02-20 17:39:35 +0100

    Print version in C++ tests (#173)
    * Print version in C++ tests
    
    * Avoiding MKL in CI
    
    * Added missing header
    
    * Fixing tests

--- test/basic/version.cpp
+++ test/basic/version.cpp
@@ -1,14 +1,20 @@
 
 #include <catch2/catch.hpp>
 #include <GooseFEM/GooseFEM.h>
+#include <iostream>
 
 TEST_CASE("GooseFEM::version", "version.h")
 {
 
     SECTION("basic")
     {
-        auto v = GooseFEM::version();
-        auto d = GooseFEM::version_dependencies();
+        std::cout << GooseFEM::version() << std::endl;
+
+        auto deps = GooseFEM::version_dependencies();
+
+        for (auto& i : deps) {
+            std::cout << i << std::endl;
+        }
     }
 
 }
