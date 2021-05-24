85dc95b7f0c24be8a83c9766282d566120a29780
Tom de Geus <tom@geus.me>
2021-05-24 17:22:21 +0200

    Allocate: convert to full-template

--- python/Allocate.hpp
+++ python/Allocate.hpp
@@ -15,16 +15,24 @@ void init_Allocate(py::module& m)
 {
     m.def("AsTensor",
           py::overload_cast<
-            size_t,
             const xt::xarray<double>&,
-            const std::vector<size_t>&>(&GooseFEM::AsTensor<xt::xarray<double>>),
+            const std::vector<size_t>&>(&GooseFEM::AsTensor<xt::xarray<double>, std::vector<size_t>>),
           "See :cpp:func:`GooseFEM::AsTensor`.",
-          py::arg("rank"),
           py::arg("arg"),
           py::arg("shape"));
 
+    m.def("AsTensor",
+          py::overload_cast<
+            size_t,
+            const xt::xarray<double>&,
+            size_t>(&GooseFEM::AsTensor<xt::xarray<double>>),
+          "See :cpp:func:`GooseFEM::AsTensor`.",
+          py::arg("rank"),
+          py::arg("arg"),
+          py::arg("n"));
+
     m.def("as3d",
-          &GooseFEM::as3d<double>,
+          &GooseFEM::as3d<xt::xarray<double>>,
           "See :cpp:func:`GooseFEM::as3d`.",
           py::arg("arg"));
 }
--- test/basic/Allocate.cpp
+++ test/basic/Allocate.cpp
@@ -9,112 +9,85 @@
 TEST_CASE("GooseFEM::Allocate", "Allocate.h")
 {
 
-    SECTION("asTensor - 1")
+    SECTION("asTensor - pre-allocated output")
     {
-        xt::xtensor<double, 2> a0 = {
-            {1.0, 2.0},
-            {3.0, 4.0}};
+        xt::xtensor<int, 2> a0 = {
+            {1, 2},
+            {3, 4}};
 
-        xt::xtensor<double, 3> a1 = {
-            {{1.0, 1.0, 1.0}, {2.0, 2.0, 2.0}},
-            {{3.0, 3.0, 3.0}, {4.0, 4.0, 4.0}}};
+        xt::xtensor<int, 3> a1 = {
+            {{1, 1, 1}, {2, 2, 2}},
+            {{3, 3, 3}, {4, 4, 4}}};
 
-        xt::xtensor<double, 4> a2 = {
-            {{{1.0, 1.0}, {1.0, 1.0}, {1.0, 1.0}}, {{2.0, 2.0}, {2.0, 2.0}, {2.0, 2.0}}},
-            {{{3.0, 3.0}, {3.0, 3.0}, {3.0, 3.0}}, {{4.0, 4.0}, {4.0, 4.0}, {4.0, 4.0}}}};
+        xt::xtensor<int, 4> a2 = {
+            {{{1, 1}, {1, 1}, {1, 1}}, {{2, 2}, {2, 2}, {2, 2}}},
+            {{{3, 3}, {3, 3}, {3, 3}}, {{4, 4}, {4, 4}, {4, 4}}}};
 
-        xt::xtensor<double, 3> b1 = xt::empty<double>({2, 2, 3});
-        xt::xtensor<double, 4> b2 = xt::empty<double>({2, 2, 3, 2});
+        xt::xtensor<int, 3> b1 = xt::empty<int>({2, 2, 3});
+        xt::xtensor<int, 4> b2 = xt::empty<int>({2, 2, 3, 2});
 
-        GooseFEM::asTensor<2, 1>(a0, b1);
-        GooseFEM::asTensor<2, 2>(a0, b2);
+        GooseFEM::asTensor(a0, b1);
+        GooseFEM::asTensor(a0, b2);
 
-        REQUIRE(xt::allclose(a1, b1));
-        REQUIRE(xt::allclose(a2, b2));
+        REQUIRE(xt::all(xt::equal(a1, b1)));
+        REQUIRE(xt::all(xt::equal(a2, b2)));
     }
 
-    SECTION("asTensor - 2")
+    SECTION("asTensor - return")
     {
-        xt::xtensor<double, 2> a0 = {
-            {1.0, 2.0},
-            {3.0, 4.0}};
+        xt::xtensor<int, 2> a0 = {
+            {1, 2},
+            {3, 4}};
 
-        xt::xtensor<double, 3> a1 = {
-            {{1.0, 1.0, 1.0}, {2.0, 2.0, 2.0}},
-            {{3.0, 3.0, 3.0}, {4.0, 4.0, 4.0}}};
+        xt::xtensor<int, 3> a1 = {
+            {{1, 1, 1}, {2, 2, 2}},
+            {{3, 3, 3}, {4, 4, 4}}};
 
-        xt::xtensor<double, 4> a2 = {
-            {{{1.0, 1.0}, {1.0, 1.0}, {1.0, 1.0}}, {{2.0, 2.0}, {2.0, 2.0}, {2.0, 2.0}}},
-            {{{3.0, 3.0}, {3.0, 3.0}, {3.0, 3.0}}, {{4.0, 4.0}, {4.0, 4.0}, {4.0, 4.0}}}};
+        xt::xtensor<int, 4> a2 = {
+            {{{1, 1}, {1, 1}, {1, 1}}, {{2, 2}, {2, 2}, {2, 2}}},
+            {{{3, 3}, {3, 3}, {3, 3}}, {{4, 4}, {4, 4}, {4, 4}}}};
 
-        auto b1 = GooseFEM::AsTensor<2, 1>(a0, 3);
-        auto b2 = GooseFEM::AsTensor<2, 2>(a0, {3, 2});
+        auto b1 = GooseFEM::AsTensor<1>(a0, 3);
+        auto c1 = GooseFEM::AsTensor(1, a0, 3);
+        auto b2 = GooseFEM::AsTensor(a0, {3, 2});
 
-        REQUIRE(xt::allclose(a1, b1));
-        REQUIRE(xt::allclose(a2, b2));
+        static_assert(std::is_same<decltype(b1), decltype(a1)>::value, "X");
+        static_assert(std::is_same<decltype(b2), decltype(a2)>::value, "X");
+        static_assert(std::is_same<decltype(c1), xt::xarray<int>>::value, "X");
+
+        REQUIRE(xt::all(xt::equal(a1, b1)));
+        REQUIRE(xt::all(xt::equal(a1, c1)));
+        REQUIRE(xt::all(xt::equal(a2, b2)));
     }
 
-    SECTION("asTensor - 3")
+    SECTION("as3d")
     {
-        xt::xtensor<double, 2> a0 = {
-            {1.0, 2.0},
-            {3.0, 4.0}};
+        xt::xtensor<int, 2> a1 = {
+            {1},
+            {3}};
 
-        xt::xtensor<double, 3> a1 = {
-            {{1.0, 1.0}, {2.0, 2.0}},
-            {{3.0, 3.0}, {4.0, 4.0}}};
+        xt::xtensor<int, 2> b1 = {
+            {1, 0, 0},
+            {3, 0, 0}};
 
-        xt::xtensor<double, 4> a2 = {
-            {{{1.0, 1.0}, {1.0, 1.0}}, {{2.0, 2.0}, {2.0, 2.0}}},
-            {{{3.0, 3.0}, {3.0, 3.0}}, {{4.0, 4.0}, {4.0, 4.0}}}};
+        xt::xtensor<int, 2> a2 = {
+            {1, 2},
+            {3, 4}};
 
-        auto b1 = GooseFEM::AsTensor<2, 1>(a0, 2);
-        auto b2 = GooseFEM::AsTensor<2, 2>(a0, 2);
+        xt::xtensor<int, 2> b2 = {
+            {1, 2, 0},
+            {3, 4, 0}};
 
-        REQUIRE(xt::allclose(a1, b1));
-        REQUIRE(xt::allclose(a2, b2));
+        xt::xtensor<int, 2> a3 = {
+            {1, 2, 5},
+            {3, 4, 6}};
+
+        xt::xtensor<int, 2> b3 = {
+            {1, 2, 5},
+            {3, 4, 6}};
+
+        REQUIRE(xt::all(xt::equal(GooseFEM::as3d(a1), b1)));
+        REQUIRE(xt::all(xt::equal(GooseFEM::as3d(a2), b2)));
+        REQUIRE(xt::all(xt::equal(GooseFEM::as3d(a3), b3)));
     }
-
-    SECTION("asTensor - 4")
-    {
-        xt::xtensor<double, 2> a0 = {
-            {1.0, 2.0},
-            {3.0, 4.0}};
-
-        xt::xtensor<double, 3> a1 = {
-            {{1.0, 1.0, 1.0}, {2.0, 2.0, 2.0}},
-            {{3.0, 3.0, 3.0}, {4.0, 4.0, 4.0}}};
-
-        xt::xtensor<double, 4> a2 = {
-            {{{1.0, 1.0}, {1.0, 1.0}, {1.0, 1.0}}, {{2.0, 2.0}, {2.0, 2.0}, {2.0, 2.0}}},
-            {{{3.0, 3.0}, {3.0, 3.0}, {3.0, 3.0}}, {{4.0, 4.0}, {4.0, 4.0}, {4.0, 4.0}}}};
-
-        auto b1 = GooseFEM::AsTensor(1, a0, 3);
-        auto b2 = GooseFEM::AsTensor(2, a0, {3, 2});
-
-        REQUIRE(xt::allclose(a1, b1));
-        REQUIRE(xt::allclose(a2, b2));
-    }
-
-    SECTION("asTensor - 5")
-    {
-        xt::xtensor<double, 2> a0 = {
-            {1.0, 2.0},
-            {3.0, 4.0}};
-
-        xt::xtensor<double, 3> a1 = {
-            {{1.0, 1.0}, {2.0, 2.0}},
-            {{3.0, 3.0}, {4.0, 4.0}}};
-
-        xt::xtensor<double, 4> a2 = {
-            {{{1.0, 1.0}, {1.0, 1.0}}, {{2.0, 2.0}, {2.0, 2.0}}},
-            {{{3.0, 3.0}, {3.0, 3.0}}, {{4.0, 4.0}, {4.0, 4.0}}}};
-
-        auto b1 = GooseFEM::AsTensor(1, a0, 2);
-        auto b2 = GooseFEM::AsTensor(2, a0, 2);
-
-        REQUIRE(xt::allclose(a1, b1));
-        REQUIRE(xt::allclose(a2, b2));
-    }
-
 }
