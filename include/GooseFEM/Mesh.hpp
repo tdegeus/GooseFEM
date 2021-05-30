da2540c5370710d6b1fe3493a962a9fd97c95657
Tom de Geus <tom@geus.me>
2021-05-30 14:07:35 +0200

    Workaround xtensor/xtensor-python bug

--- include/GooseFEM/Mesh.hpp
+++ include/GooseFEM/Mesh.hpp
@@ -1571,7 +1571,9 @@ inline Reorder::Reorder(const std::initializer_list<T> args)
 
     #ifdef GOOSEFEM_ENABLE_ASSERT
     for (auto& arg : args) {
-        GOOSEFEM_ASSERT(xt::unique(arg) == xt::sort(arg));
+        // todo: remove tmp when https://github.com/xtensor-stack/xtensor-python/issues/262
+        xt::xtensor<size_t, 1> tmp = xt::ravel(arg);
+        GOOSEFEM_ASSERT(xt::unique(tmp) == xt::sort(tmp));
     }
     #endif
 
