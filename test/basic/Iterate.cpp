aeb03c79df249ee2b1a1edad8326698e8ab4be6d
Tom de Geus <tom@geus.me>
2021-06-01 18:48:37 +0200

    Fixing tests

--- test/basic/Iterate.cpp
+++ test/basic/Iterate.cpp
@@ -9,6 +9,34 @@
 
 TEST_CASE("GooseFEM::Iterate", "Iterate.h")
 {
+    SECTION("StopList::stop_simple - sorted input")
+    {
+        GooseFEM::Iterate::StopList stop(5);
+
+        std::vector<double> res = {5e+0,  5e+1,  5e-1,  5e-2,  5e-3,  5e-4,  4e-4,  3e-4,  2e-4,  1e-4};
+        std::vector<bool> conv =  {false, false, false, false, false, false, false, false, false, true};
+
+        for (size_t i = 0; i < res.size(); ++i) {
+            REQUIRE(stop.stop_simple(res[i], 1e-3) == conv[i]);
+        }
+
+        std::vector<double> m_res = { 0.05  ,  0.005 ,  0.0005,  0.0004,  0.0005};
+    }
+
+    SECTION("StopList::stop_simple - unsorted input")
+    {
+        GooseFEM::Iterate::StopList a(5);
+        GooseFEM::Iterate::StopList b(5);
+
+        std::vector<double> res = {5e+0,  5e+1,  5e-1,  5e-2,  5e-3,  5e-4,  4e-4,  5e-4,  4e-4,  4e-4};
+        std::vector<bool> conv =  {false, false, false, false, false, false, false, false, false, true};
+
+        for (size_t i = 0; i < res.size(); ++i) {
+            REQUIRE(a.stop_simple(res[i], 1e-3) == conv[i]);
+            REQUIRE(b.stop(res[i], 1e-3) == false);
+        }
+    }
+
     SECTION("StopList::stop - sorted input")
     {
         GooseFEM::Iterate::StopList stop(5);
@@ -19,37 +47,9 @@ TEST_CASE("GooseFEM::Iterate", "Iterate.h")
         for (size_t i = 0; i < res.size(); ++i) {
             REQUIRE(stop.stop(res[i], 1e-3) == conv[i]);
         }
-
-        std::vector<double> m_res = { 0.05  ,  0.005 ,  0.0005,  0.0004,  0.0005};
     }
 
     SECTION("StopList::stop - unsorted input")
-    {
-        GooseFEM::Iterate::StopList a(5);
-        GooseFEM::Iterate::StopList b(5);
-
-        std::vector<double> res = {5e+0,  5e+1,  5e-1,  5e-2,  5e-3,  5e-4,  4e-4,  5e-4,  4e-4,  4e-4};
-        std::vector<bool> conv =  {false, false, false, false, false, false, false, false, false, true};
-
-        for (size_t i = 0; i < res.size(); ++i) {
-            REQUIRE(a.stop(res[i], 1e-3) == conv[i]);
-            REQUIRE(b.stop_sorted(res[i], 1e-3) == false);
-        }
-    }
-
-    SECTION("StopList::stop_sorted - sorted input")
-    {
-        GooseFEM::Iterate::StopList stop(5);
-
-        std::vector<double> res = {5e+0,  5e+1,  5e-1,  5e-2,  5e-3,  5e-4,  4e-4,  3e-4,  2e-4,  1e-4};
-        std::vector<bool> conv =  {false, false, false, false, false, false, false, false, false, true};
-
-        for (size_t i = 0; i < res.size(); ++i) {
-            REQUIRE(stop.stop_sorted(res[i], 1e-3) == conv[i]);
-        }
-    }
-
-    SECTION("StopList::stop_sorted - unsorted input")
     {
         GooseFEM::Iterate::StopList stop(5);
 
@@ -57,7 +57,7 @@ TEST_CASE("GooseFEM::Iterate", "Iterate.h")
         std::vector<bool> conv =  {false, false, false, false, false, false, false, false, false, false, false, false, false, true};
 
         for (size_t i = 0; i < res.size(); ++i) {
-            REQUIRE(stop.stop_sorted(res[i], 1e-3) == conv[i]);
+            REQUIRE(stop.stop(res[i], 1e-3) == conv[i]);
         }
     }
 }
