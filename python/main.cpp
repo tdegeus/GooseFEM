73b104caa7b7552cba430c13fde10b55bbf0b32a
Tom de Geus <tom@geus.me>
2022-02-18 16:31:54 +0100

    Fixing name in Python module

--- python/main.cpp
+++ python/main.cpp
@@ -38,8 +38,32 @@
 
 namespace py = pybind11;
 
+/**
+Overrides the `__name__` of a module.
+Classes defined by pybind11 use the `__name__` of the module as of the time they are defined,
+which affects the `__repr__` of the class type objects.
+*/
+class ScopedModuleNameOverride {
+public:
+    explicit ScopedModuleNameOverride(py::module m, std::string name) : module_(std::move(m))
+    {
+        original_name_ = module_.attr("__name__");
+        module_.attr("__name__") = name;
+    }
+    ~ScopedModuleNameOverride()
+    {
+        module_.attr("__name__") = original_name_;
+    }
+
+private:
+    py::module module_;
+    py::object original_name_;
+};
+
 PYBIND11_MODULE(_GooseFEM, m)
 {
+    // Ensure members to display as `GooseFEM.X` (not `GooseFEM._GooseFEM.X`)
+    ScopedModuleNameOverride name_override(m, "GooseFEM");
 
     xt::import_numpy();
 
