6d5ffaf939219896049b97da11b2df9091c9dd78
Tom de Geus <tom@geus.me>
2022-07-08 19:17:09 +0200

    TODO: Work around GMat bug until upstream is fixed

--- docs/examples/statics/Periodic_LinearElastic/main.cpp
+++ docs/examples/statics/Periodic_LinearElastic/main.cpp
@@ -1,3 +1,42 @@
+// todo remove
+#include <xtensor/xtensor.hpp>
+
+namespace GMatTensor {
+namespace detail {
+
+template <size_t RANK, class T>
+struct allocate {
+};
+
+template <size_t RANK, class EC, size_t N, xt::layout_type L, class Tag>
+struct allocate<RANK, xt::xtensor<EC, N, L, Tag>> {
+    using type = typename xt::xtensor<EC, RANK, L, Tag>;
+};
+
+#ifdef XTENSOR_FIXED_HPP
+template <size_t RANK, class EC, class S, xt::layout_type L>
+struct allocate<RANK, xt::xtensor_fixed<EC, S, L>> {
+    using type = typename xt::xtensor<EC, RANK, L>;
+};
+#endif
+
+#ifdef XTENSOR_FIXED_HPP
+template <size_t RANK, class EC, class S, xt::layout_type L, bool SH, class Tag>
+struct allocate<RANK, xt::xfixed_container<EC, S, L, SH, Tag>> {
+    using type = typename xt::xtensor<EC, RANK, L, Tag>;
+};
+#endif
+
+#ifdef PY_TENSOR_HPP
+template <size_t RANK, class EC, size_t N, xt::layout_type L>
+struct allocate<RANK, xt::pytensor<EC, N, L>> {
+    using type = typename xt::pytensor<EC, RANK, L>;
+};
+#endif
+
+} // namespace detail
+} // namespace GMatTensor
+
 #include <Eigen/Eigen>
 #include <GMatElastic/Cartesian3d.h>
 #include <GooseFEM/GooseFEM.h>
