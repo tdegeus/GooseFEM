51fab6a4b7b1b55f5b6f95a6404e331a7e9134c9
Tom de Geus <tom@geus.me>
2022-07-14 17:25:25 +0200

    assertions: merging implementation and header

--- include/GooseFEM/assertions.h
+++ include/GooseFEM/assertions.h
@@ -1,10 +1,8 @@
 /**
- *  Common assertions with verbosity.
- *
- *  \file assertions.h
- *  \copyright Copyright 2017. Tom de Geus. All rights reserved.
- *  \license This project is released under the GNU Public License (GPLv3).
- */
+\file
+\copyright Copyright 2017. Tom de Geus. All rights reserved.
+\license This project is released under the GNU Public License (GPLv3).
+*/
 
 #ifndef GOOSEFEM_ASSERTIONS_H
 #define GOOSEFEM_ASSERTIONS_H
@@ -14,15 +12,17 @@
 namespace GooseFEM {
 
 /**
- *  Returns true is a list is unique (has not duplicate items).
- *  \param a Array-like.
- *  \return `true` if `a` is unique.
- */
+Returns true is a list is unique (has not duplicate items).
+\param arg Array-like.
+\return `true` if `arg` is unique.
+*/
 template <class T>
-inline bool is_unique(const T& a);
+inline bool is_unique(const T& arg)
+{
+    array_type::tensor<typename T::value_type, 1> tmp = xt::ravel(arg);
+    return xt::all(xt::equal(xt::unique(tmp), xt::sort(tmp)));
+}
 
 } // namespace GooseFEM
-
-#include "assertions.hpp"
 
 #endif
