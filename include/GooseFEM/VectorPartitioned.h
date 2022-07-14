d3dd1d72a4c9806d67c7ec3feddfde738e1986a1
Tom de Geus <tom@geus.me>
2022-07-14 15:37:40 +0200

    VectorPartitioned: merging implementation and header

--- include/GooseFEM/VectorPartitioned.h
+++ include/GooseFEM/VectorPartitioned.h
@@ -11,7 +11,9 @@ Methods to switch between storage types based on a mesh and DOFs that are partit
 #ifndef GOOSEFEM_VECTORPARTITIONED_H
 #define GOOSEFEM_VECTORPARTITIONED_H
 
+#include "Mesh.h"
 #include "Vector.h"
+#include "assertions.h"
 #include "config.h"
 
 namespace GooseFEM {
@@ -42,6 +44,22 @@ namespace GooseFEM {
  *
  */
 class VectorPartitioned : public Vector {
+protected:
+    array_type::tensor<size_t, 1> m_iiu; ///< See iiu()
+    array_type::tensor<size_t, 1> m_iip; ///< See iip()
+    size_t m_nnu; ///< See #nnu
+    size_t m_nnp; ///< See #nnp
+
+    /**
+    Renumbered DOFs per node, such that
+
+        iiu = arange(nnu)
+        iip = nnu + arange(nnp)
+
+    making is much simpler to slice.
+    */
+    array_type::tensor<size_t, 2> m_part;
+
 public:
     VectorPartitioned() = default;
 
@@ -55,41 +73,92 @@ public:
     VectorPartitioned(
         const array_type::tensor<size_t, 2>& conn,
         const array_type::tensor<size_t, 2>& dofs,
-        const array_type::tensor<size_t, 1>& iip);
+        const array_type::tensor<size_t, 1>& iip)
+        : Vector(conn, dofs), m_iip(iip)
+    {
+        GOOSEFEM_ASSERT(is_unique(iip));
+
+        m_iiu = xt::setdiff1d(m_dofs, m_iip);
+        m_nnp = m_iip.size();
+        m_nnu = m_iiu.size();
+        m_part = Mesh::Reorder({m_iiu, m_iip}).apply(m_dofs);
+
+        GOOSEFEM_ASSERT(xt::amax(m_iip)() <= xt::amax(m_dofs)());
+    }
 
     /**
     \return Number of unknown DOFs.
     */
-    size_t nnu() const;
+    size_t nnu() const
+    {
+        return m_nnu;
+    }
 
     /**
     \return Number of prescribed DOFs.
     */
-    size_t nnp() const;
+    size_t nnp() const
+    {
+        return m_nnp;
+    }
 
     /**
     \return Unknown DOFs [#nnu].
     */
-    array_type::tensor<size_t, 1> iiu() const;
+    array_type::tensor<size_t, 1> iiu() const
+    {
+        return m_iiu;
+    }
 
     /**
     \return Prescribed DOFs [#nnp].
     */
-    array_type::tensor<size_t, 1> iip() const;
+    array_type::tensor<size_t, 1> iip() const
+    {
+        return m_iip;
+    }
 
     /**
     Per DOF (see Vector::dofs()) list if unknown ("u").
 
     \return Boolean "nodevec".
     */
-    array_type::tensor<bool, 2> dofs_is_u() const;
+    array_type::tensor<bool, 2> dofs_is_u() const
+    {
+        array_type::tensor<bool, 2> ret = xt::zeros<bool>(this->shape_nodevec());
+
+#pragma omp parallel for
+        for (size_t m = 0; m < m_nnode; ++m) {
+            for (size_t i = 0; i < m_ndim; ++i) {
+                if (m_part(m, i) < m_nnu) {
+                    ret(m, i) = true;
+                }
+            }
+        }
+
+        return ret;
+    }
 
     /**
     Per DOF (see Vector::dofs()) list if prescribed ("p").
 
     \return Boolean "nodevec".
     */
-    array_type::tensor<bool, 2> dofs_is_p() const;
+    array_type::tensor<bool, 2> dofs_is_p() const
+    {
+        array_type::tensor<bool, 2> ret = xt::zeros<bool>(this->shape_nodevec());
+
+#pragma omp parallel for
+        for (size_t m = 0; m < m_nnode; ++m) {
+            for (size_t i = 0; i < m_ndim; ++i) {
+                if (m_part(m, i) >= m_nnu) {
+                    ret(m, i) = true;
+                }
+            }
+        }
+
+        return ret;
+    }
 
     /**
     Copy unknown DOFs from "nodevec" to another "nodvec":
@@ -106,7 +175,12 @@ public:
     */
     array_type::tensor<double, 2> Copy_u(
         const array_type::tensor<double, 2>& nodevec_src,
-        const array_type::tensor<double, 2>& nodevec_dest) const;
+        const array_type::tensor<double, 2>& nodevec_dest) const
+    {
+        array_type::tensor<double, 2> ret = nodevec_dest;
+        this->copy_u(nodevec_src, ret);
+        return ret;
+    }
 
     /**
     Copy unknown DOFs from "nodevec" to another "nodvec":
@@ -122,7 +196,20 @@ public:
     */
     void copy_u(
         const array_type::tensor<double, 2>& nodevec_src,
-        array_type::tensor<double, 2>& nodevec_dest) const;
+        array_type::tensor<double, 2>& nodevec_dest) const
+    {
+        GOOSEFEM_ASSERT(xt::has_shape(nodevec_src, {m_nnode, m_ndim}));
+        GOOSEFEM_ASSERT(xt::has_shape(nodevec_dest, {m_nnode, m_ndim}));
+
+#pragma omp parallel for
+        for (size_t m = 0; m < m_nnode; ++m) {
+            for (size_t i = 0; i < m_ndim; ++i) {
+                if (m_part(m, i) < m_nnu) {
+                    nodevec_dest(m, i) = nodevec_src(m, i);
+                }
+            }
+        }
+    }
 
     /**
     Copy prescribed DOFs from "nodevec" to another "nodvec":
@@ -139,7 +226,12 @@ public:
     */
     array_type::tensor<double, 2> Copy_p(
         const array_type::tensor<double, 2>& nodevec_src,
-        const array_type::tensor<double, 2>& nodevec_dest) const;
+        const array_type::tensor<double, 2>& nodevec_dest) const
+    {
+        array_type::tensor<double, 2> ret = nodevec_dest;
+        this->copy_p(nodevec_src, ret);
+        return ret;
+    }
 
     /**
     Copy prescribed DOFs from "nodevec" to another "nodvec":
@@ -155,7 +247,20 @@ public:
     */
     void copy_p(
         const array_type::tensor<double, 2>& nodevec_src,
-        array_type::tensor<double, 2>& nodevec_dest) const;
+        array_type::tensor<double, 2>& nodevec_dest) const
+    {
+        GOOSEFEM_ASSERT(xt::has_shape(nodevec_src, {m_nnode, m_ndim}));
+        GOOSEFEM_ASSERT(xt::has_shape(nodevec_dest, {m_nnode, m_ndim}));
+
+#pragma omp parallel for
+        for (size_t m = 0; m < m_nnode; ++m) {
+            for (size_t i = 0; i < m_ndim; ++i) {
+                if (m_part(m, i) >= m_nnu) {
+                    nodevec_dest(m, i) = nodevec_src(m, i);
+                }
+            }
+        }
+    }
 
     /**
     Combine unknown and prescribed "dofval" into a single "dofval" list.
@@ -166,7 +271,12 @@ public:
     */
     array_type::tensor<double, 1> DofsFromParitioned(
         const array_type::tensor<double, 1>& dofval_u,
-        const array_type::tensor<double, 1>& dofval_p) const;
+        const array_type::tensor<double, 1>& dofval_p) const
+    {
+        array_type::tensor<double, 1> dofval = xt::empty<double>({m_ndof});
+        this->dofsFromParitioned(dofval_u, dofval_p, dofval);
+        return dofval;
+    }
 
     /**
     Combine unknown and prescribed "dofval" into a single "dofval" list.
@@ -178,7 +288,24 @@ public:
     void dofsFromParitioned(
         const array_type::tensor<double, 1>& dofval_u,
         const array_type::tensor<double, 1>& dofval_p,
-        array_type::tensor<double, 1>& dofval) const;
+        array_type::tensor<double, 1>& dofval) const
+    {
+        GOOSEFEM_ASSERT(dofval_u.size() == m_nnu);
+        GOOSEFEM_ASSERT(dofval_p.size() == m_nnp);
+        GOOSEFEM_ASSERT(dofval.size() == m_ndof);
+
+        dofval.fill(0.0);
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnu; ++d) {
+            dofval(m_iiu(d)) = dofval_u(d);
+        }
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnp; ++d) {
+            dofval(m_iip(d)) = dofval_p(d);
+        }
+    }
 
     /**
     Combine unknown and prescribed "dofval" into a single "dofval" list
@@ -191,7 +318,12 @@ public:
     */
     array_type::tensor<double, 2> NodeFromPartitioned(
         const array_type::tensor<double, 1>& dofval_u,
-        const array_type::tensor<double, 1>& dofval_p) const;
+        const array_type::tensor<double, 1>& dofval_p) const
+    {
+        array_type::tensor<double, 2> nodevec = xt::empty<double>({m_nnode, m_ndim});
+        this->nodeFromPartitioned(dofval_u, dofval_p, nodevec);
+        return nodevec;
+    }
 
     /**
     Combine unknown and prescribed "dofval" into a single "dofval" list
@@ -205,7 +337,24 @@ public:
     void nodeFromPartitioned(
         const array_type::tensor<double, 1>& dofval_u,
         const array_type::tensor<double, 1>& dofval_p,
-        array_type::tensor<double, 2>& nodevec) const;
+        array_type::tensor<double, 2>& nodevec) const
+    {
+        GOOSEFEM_ASSERT(dofval_u.size() == m_nnu);
+        GOOSEFEM_ASSERT(dofval_p.size() == m_nnp);
+        GOOSEFEM_ASSERT(xt::has_shape(nodevec, {m_nnode, m_ndim}));
+
+#pragma omp parallel for
+        for (size_t m = 0; m < m_nnode; ++m) {
+            for (size_t i = 0; i < m_ndim; ++i) {
+                if (m_part(m, i) < m_nnu) {
+                    nodevec(m, i) = dofval_u(m_part(m, i));
+                }
+                else {
+                    nodevec(m, i) = dofval_p(m_part(m, i) - m_nnu);
+                }
+            }
+        }
+    }
 
     /**
     Combine unknown and prescribed "dofval" into a single "dofval" list
@@ -218,7 +367,12 @@ public:
     */
     array_type::tensor<double, 3> ElementFromPartitioned(
         const array_type::tensor<double, 1>& dofval_u,
-        const array_type::tensor<double, 1>& dofval_p) const;
+        const array_type::tensor<double, 1>& dofval_p) const
+    {
+        array_type::tensor<double, 3> elemvec = xt::empty<double>({m_nelem, m_nne, m_ndim});
+        this->elementFromPartitioned(dofval_u, dofval_p, elemvec);
+        return elemvec;
+    }
 
     /**
     Combine unknown and prescribed "dofval" into a single "dofval" list
@@ -232,7 +386,26 @@ public:
     void elementFromPartitioned(
         const array_type::tensor<double, 1>& dofval_u,
         const array_type::tensor<double, 1>& dofval_p,
-        array_type::tensor<double, 3>& elemvec) const;
+        array_type::tensor<double, 3>& elemvec) const
+    {
+        GOOSEFEM_ASSERT(dofval_u.size() == m_nnu);
+        GOOSEFEM_ASSERT(dofval_p.size() == m_nnp);
+        GOOSEFEM_ASSERT(xt::has_shape(elemvec, {m_nelem, m_nne, m_ndim}));
+
+#pragma omp parallel for
+        for (size_t e = 0; e < m_nelem; ++e) {
+            for (size_t m = 0; m < m_nne; ++m) {
+                for (size_t i = 0; i < m_ndim; ++i) {
+                    if (m_part(m_conn(e, m), i) < m_nnu) {
+                        elemvec(e, m, i) = dofval_u(m_part(m_conn(e, m), i));
+                    }
+                    else {
+                        elemvec(e, m, i) = dofval_p(m_part(m_conn(e, m), i) - m_nnu);
+                    }
+                }
+            }
+        }
+    }
 
     /**
     Extract the unknown "dofval":
@@ -242,7 +415,12 @@ public:
     \param dofval input [#ndof]
     \return dofval_u input [#nnu]
     */
-    array_type::tensor<double, 1> AsDofs_u(const array_type::tensor<double, 1>& dofval) const;
+    array_type::tensor<double, 1> AsDofs_u(const array_type::tensor<double, 1>& dofval) const
+    {
+        array_type::tensor<double, 1> dofval_u = xt::empty<double>({m_nnu});
+        this->asDofs_u(dofval, dofval_u);
+        return dofval_u;
+    }
 
     /**
     Extract the unknown "dofval":
@@ -254,7 +432,16 @@ public:
     */
     void asDofs_u(
         const array_type::tensor<double, 1>& dofval,
-        array_type::tensor<double, 1>& dofval_u) const;
+        array_type::tensor<double, 1>& dofval_u) const
+    {
+        GOOSEFEM_ASSERT(dofval.size() == m_ndof);
+        GOOSEFEM_ASSERT(dofval_u.size() == m_nnu);
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnu; ++d) {
+            dofval_u(d) = dofval(m_iiu(d));
+        }
+    }
 
     /**
     Convert "nodevec" to "dofval" (overwrite entries that occur more than once)
@@ -263,7 +450,12 @@ public:
     \param nodevec input [#nnode, #ndim]
     \return dofval_u input [#nnu]
     */
-    array_type::tensor<double, 1> AsDofs_u(const array_type::tensor<double, 2>& nodevec) const;
+    array_type::tensor<double, 1> AsDofs_u(const array_type::tensor<double, 2>& nodevec) const
+    {
+        array_type::tensor<double, 1> dofval_u = xt::empty<double>({m_nnu});
+        this->asDofs_u(nodevec, dofval_u);
+        return dofval_u;
+    }
 
     /**
     Convert "nodevec" to "dofval" (overwrite entries that occur more than once)
@@ -274,7 +466,22 @@ public:
     */
     void asDofs_u(
         const array_type::tensor<double, 2>& nodevec,
-        array_type::tensor<double, 1>& dofval_u) const;
+        array_type::tensor<double, 1>& dofval_u) const
+    {
+        GOOSEFEM_ASSERT(xt::has_shape(nodevec, {m_nnode, m_ndim}));
+        GOOSEFEM_ASSERT(dofval_u.size() == m_nnu);
+
+        dofval_u.fill(0.0);
+
+#pragma omp parallel for
+        for (size_t m = 0; m < m_nnode; ++m) {
+            for (size_t i = 0; i < m_ndim; ++i) {
+                if (m_part(m, i) < m_nnu) {
+                    dofval_u(m_part(m, i)) = nodevec(m, i);
+                }
+            }
+        }
+    }
 
     /**
     Convert "elemvec" to "dofval" (overwrite entries that occur more than once)
@@ -283,7 +490,12 @@ public:
     \param elemvec input [#nelem, #nne, #ndim]
     \return dofval_u input [#nnu]
     */
-    array_type::tensor<double, 1> AsDofs_u(const array_type::tensor<double, 3>& elemvec) const;
+    array_type::tensor<double, 1> AsDofs_u(const array_type::tensor<double, 3>& elemvec) const
+    {
+        array_type::tensor<double, 1> dofval_u = xt::empty<double>({m_nnu});
+        this->asDofs_u(elemvec, dofval_u);
+        return dofval_u;
+    }
 
     /**
     Convert "elemvec" to "dofval" (overwrite entries that occur more than once)
@@ -294,7 +506,24 @@ public:
     */
     void asDofs_u(
         const array_type::tensor<double, 3>& elemvec,
-        array_type::tensor<double, 1>& dofval_u) const;
+        array_type::tensor<double, 1>& dofval_u) const
+    {
+        GOOSEFEM_ASSERT(xt::has_shape(elemvec, {m_nelem, m_nne, m_ndim}));
+        GOOSEFEM_ASSERT(dofval_u.size() == m_nnu);
+
+        dofval_u.fill(0.0);
+
+#pragma omp parallel for
+        for (size_t e = 0; e < m_nelem; ++e) {
+            for (size_t m = 0; m < m_nne; ++m) {
+                for (size_t i = 0; i < m_ndim; ++i) {
+                    if (m_part(m_conn(e, m), i) < m_nnu) {
+                        dofval_u(m_part(m_conn(e, m), i)) = elemvec(e, m, i);
+                    }
+                }
+            }
+        }
+    }
 
     /**
     Extract the prescribed "dofval":
@@ -304,7 +533,12 @@ public:
     \param dofval input [#ndof]
     \return dofval_p input [#nnp]
     */
-    array_type::tensor<double, 1> AsDofs_p(const array_type::tensor<double, 1>& dofval) const;
+    array_type::tensor<double, 1> AsDofs_p(const array_type::tensor<double, 1>& dofval) const
+    {
+        array_type::tensor<double, 1> dofval_p = xt::empty<double>({m_nnp});
+        this->asDofs_p(dofval, dofval_p);
+        return dofval_p;
+    }
 
     /**
     Extract the prescribed "dofval":
@@ -316,7 +550,16 @@ public:
     */
     void asDofs_p(
         const array_type::tensor<double, 1>& dofval,
-        array_type::tensor<double, 1>& dofval_p) const;
+        array_type::tensor<double, 1>& dofval_p) const
+    {
+        GOOSEFEM_ASSERT(dofval.size() == m_ndof);
+        GOOSEFEM_ASSERT(dofval_p.size() == m_nnp);
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnp; ++d) {
+            dofval_p(d) = dofval(m_iip(d));
+        }
+    }
 
     /**
     Convert "nodevec" to "dofval" (overwrite entries that occur more than once)
@@ -325,7 +568,12 @@ public:
     \param nodevec input [#nnode, #ndim]
     \return dofval_p input [#nnp]
     */
-    array_type::tensor<double, 1> AsDofs_p(const array_type::tensor<double, 2>& nodevec) const;
+    array_type::tensor<double, 1> AsDofs_p(const array_type::tensor<double, 2>& nodevec) const
+    {
+        array_type::tensor<double, 1> dofval_p = xt::empty<double>({m_nnp});
+        this->asDofs_p(nodevec, dofval_p);
+        return dofval_p;
+    }
 
     /**
     Convert "nodevec" to "dofval" (overwrite entries that occur more than once)
@@ -336,7 +584,22 @@ public:
     */
     void asDofs_p(
         const array_type::tensor<double, 2>& nodevec,
-        array_type::tensor<double, 1>& dofval_p) const;
+        array_type::tensor<double, 1>& dofval_p) const
+    {
+        GOOSEFEM_ASSERT(xt::has_shape(nodevec, {m_nnode, m_ndim}));
+        GOOSEFEM_ASSERT(dofval_p.size() == m_nnp);
+
+        dofval_p.fill(0.0);
+
+#pragma omp parallel for
+        for (size_t m = 0; m < m_nnode; ++m) {
+            for (size_t i = 0; i < m_ndim; ++i) {
+                if (m_part(m, i) >= m_nnu) {
+                    dofval_p(m_part(m, i) - m_nnu) = nodevec(m, i);
+                }
+            }
+        }
+    }
 
     /**
     Convert "elemvec" to "dofval" (overwrite entries that occur more than once)
@@ -345,7 +608,12 @@ public:
     \param elemvec input [#nelem, #nne, #ndim]
     \return dofval_p input [#nnp]
     */
-    array_type::tensor<double, 1> AsDofs_p(const array_type::tensor<double, 3>& elemvec) const;
+    array_type::tensor<double, 1> AsDofs_p(const array_type::tensor<double, 3>& elemvec) const
+    {
+        array_type::tensor<double, 1> dofval_p = xt::empty<double>({m_nnp});
+        this->asDofs_p(elemvec, dofval_p);
+        return dofval_p;
+    }
 
     /**
     Convert "elemvec" to "dofval" (overwrite entries that occur more than once)
@@ -356,27 +624,26 @@ public:
     */
     void asDofs_p(
         const array_type::tensor<double, 3>& elemvec,
-        array_type::tensor<double, 1>& dofval_p) const;
+        array_type::tensor<double, 1>& dofval_p) const
+    {
+        GOOSEFEM_ASSERT(xt::has_shape(elemvec, {m_nelem, m_nne, m_ndim}));
+        GOOSEFEM_ASSERT(dofval_p.size() == m_nnp);
 
-protected:
-    array_type::tensor<size_t, 1> m_iiu; ///< See iiu()
-    array_type::tensor<size_t, 1> m_iip; ///< See iip()
-    size_t m_nnu; ///< See #nnu
-    size_t m_nnp; ///< See #nnp
+        dofval_p.fill(0.0);
 
-    /**
-    Renumbered DOFs per node, such that
-
-        iiu = arange(nnu)
-        iip = nnu + arange(nnp)
-
-    making is much simpler to slice.
-    */
-    array_type::tensor<size_t, 2> m_part;
+#pragma omp parallel for
+        for (size_t e = 0; e < m_nelem; ++e) {
+            for (size_t m = 0; m < m_nne; ++m) {
+                for (size_t i = 0; i < m_ndim; ++i) {
+                    if (m_part(m_conn(e, m), i) >= m_nnu) {
+                        dofval_p(m_part(m_conn(e, m), i) - m_nnu) = elemvec(e, m, i);
+                    }
+                }
+            }
+        }
+    }
 };
 
 } // namespace GooseFEM
-
-#include "VectorPartitioned.hpp"
 
 #endif
