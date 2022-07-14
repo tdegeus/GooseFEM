dad7a72816292b7417756faf45ae1eeb269ef6be
Tom de Geus <tom@geus.me>
2022-07-14 18:29:33 +0200

    MatrixDiagonalPartitioned: merging implementation and header

--- include/GooseFEM/MatrixDiagonalPartitioned.h
+++ include/GooseFEM/MatrixDiagonalPartitioned.h
@@ -12,6 +12,7 @@ Diagonal matrix that is partitioned in:
 #define GOOSEFEM_MATRIXDIAGONALPARTITIONED_H
 
 #include "MatrixDiagonal.h"
+#include "Mesh.h"
 #include "config.h"
 
 namespace GooseFEM {
@@ -35,38 +36,162 @@ public:
     MatrixDiagonalPartitioned(
         const array_type::tensor<size_t, 2>& conn,
         const array_type::tensor<size_t, 2>& dofs,
-        const array_type::tensor<size_t, 1>& iip);
+        const array_type::tensor<size_t, 1>& iip)
+    {
+        m_conn = conn;
+        m_dofs = dofs;
+        m_nelem = m_conn.shape(0);
+        m_nne = m_conn.shape(1);
+        m_nnode = m_dofs.shape(0);
+        m_ndim = m_dofs.shape(1);
+        m_ndof = xt::amax(m_dofs)() + 1;
+
+        GOOSEFEM_ASSERT(is_unique(iip));
+        GOOSEFEM_ASSERT(xt::amax(conn)() + 1 <= m_nnode);
+        GOOSEFEM_ASSERT(m_ndof <= m_nnode * m_ndim);
+        GOOSEFEM_ASSERT(xt::amax(iip)() <= xt::amax(dofs)());
+
+        m_iip = iip;
+        m_iiu = xt::setdiff1d(dofs, iip);
+        m_nnp = m_iip.size();
+        m_nnu = m_iiu.size();
+        m_part = Mesh::Reorder({m_iiu, m_iip}).apply(m_dofs);
+        m_Auu = xt::empty<double>({m_nnu});
+        m_App = xt::empty<double>({m_nnp});
+        m_inv_uu = xt::empty<double>({m_nnu});
+    }
 
     /**
     Number of unknown DOFs.
     */
-    size_t nnu() const;
+    size_t nnu() const
+    {
+        return m_nnu;
+    }
 
     /**
     Number of prescribed DOFs.
     */
-    size_t nnp() const;
+    size_t nnp() const
+    {
+        return m_nnp;
+    }
 
     /**
     Unknown DOFs [#nnu].
     */
-    array_type::tensor<size_t, 1> iiu() const;
+    array_type::tensor<size_t, 1> iiu() const
+    {
+        return m_iiu;
+    }
 
     /**
     Prescribed DOFs [#nnp].
     */
-    array_type::tensor<size_t, 1> iip() const;
+    array_type::tensor<size_t, 1> iip() const
+    {
+        return m_iip;
+    }
 
-    void assemble(const array_type::tensor<double, 3>& elemmat) override;
+    void assemble(const array_type::tensor<double, 3>& elemmat) override
+    {
+        GOOSEFEM_ASSERT(xt::has_shape(elemmat, {m_nelem, m_nne * m_ndim, m_nne * m_ndim}));
+        GOOSEFEM_ASSERT(Element::isDiagonal(elemmat));
 
-    void set(const array_type::tensor<double, 1>& A) override;
+        m_Auu.fill(0.0);
+        m_App.fill(0.0);
 
-    array_type::tensor<double, 1> Todiagonal() const override;
+        for (size_t e = 0; e < m_nelem; ++e) {
+            for (size_t m = 0; m < m_nne; ++m) {
+                for (size_t i = 0; i < m_ndim; ++i) {
+
+                    size_t d = m_part(m_conn(e, m), i);
+
+                    if (d < m_nnu) {
+                        m_Auu(d) += elemmat(e, m * m_ndim + i, m * m_ndim + i);
+                    }
+                    else {
+                        m_App(d - m_nnu) += elemmat(e, m * m_ndim + i, m * m_ndim + i);
+                    }
+                }
+            }
+        }
+
+        m_changed = true;
+    }
+
+    void set(const array_type::tensor<double, 1>& A) override
+    {
+        GOOSEFEM_ASSERT(A.size() == m_ndof);
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnu; ++d) {
+            m_Auu(d) = A(m_iiu(d));
+        }
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnp; ++d) {
+            m_App(d) = A(m_iip(d));
+        }
+
+        m_changed = true;
+    }
+
+    array_type::tensor<double, 1> Todiagonal() const override
+    {
+        array_type::tensor<double, 1> ret = xt::zeros<double>({m_ndof});
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnu; ++d) {
+            ret(m_iiu(d)) = m_Auu(d);
+        }
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnp; ++d) {
+            ret(m_iip(d)) = m_App(d);
+        }
+
+        return ret;
+    }
 
     void
-    dot(const array_type::tensor<double, 2>& x, array_type::tensor<double, 2>& b) const override;
+    dot(const array_type::tensor<double, 2>& x, array_type::tensor<double, 2>& b) const override
+    {
+        GOOSEFEM_ASSERT(xt::has_shape(x, {m_nnode, m_ndim}));
+        GOOSEFEM_ASSERT(xt::has_shape(b, {m_nnode, m_ndim}));
+
+#pragma omp parallel for
+        for (size_t m = 0; m < m_nnode; ++m) {
+            for (size_t i = 0; i < m_ndim; ++i) {
+
+                size_t d = m_part(m, i);
+
+                if (d < m_nnu) {
+                    b(m, i) = m_Auu(d) * x(m, i);
+                }
+                else {
+                    b(m, i) = m_App(d - m_nnu) * x(m, i);
+                }
+            }
+        }
+    }
+
     void
-    dot(const array_type::tensor<double, 1>& x, array_type::tensor<double, 1>& b) const override;
+    dot(const array_type::tensor<double, 1>& x, array_type::tensor<double, 1>& b) const override
+    {
+        GOOSEFEM_ASSERT(x.size() == m_ndof);
+        GOOSEFEM_ASSERT(b.size() == m_ndof);
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnu; ++d) {
+            b(m_iiu(d)) = m_Auu(d) * x(m_iiu(d));
+        }
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnp; ++d) {
+            b(m_iip(d)) = m_App(d) * x(m_iip(d));
+        }
+    }
 
     /**
     \param x_u dofval [#nnu].
@@ -74,7 +199,12 @@ public:
     \return b_u dofval [#nnu].
     */
     array_type::tensor<double, 1>
-    Dot_u(const array_type::tensor<double, 1>& x_u, const array_type::tensor<double, 1>& x_p) const;
+    Dot_u(const array_type::tensor<double, 1>& x_u, const array_type::tensor<double, 1>& x_p) const
+    {
+        array_type::tensor<double, 1> b_u = xt::empty<double>({m_nnu});
+        this->dot_u(x_u, x_p, b_u);
+        return b_u;
+    }
 
     /**
     \param x_u dofval [#nnu].
@@ -84,7 +214,19 @@ public:
     void dot_u(
         const array_type::tensor<double, 1>& x_u,
         const array_type::tensor<double, 1>& x_p,
-        array_type::tensor<double, 1>& b_u) const;
+        array_type::tensor<double, 1>& b_u) const
+    {
+        UNUSED(x_p);
+
+        GOOSEFEM_ASSERT(x_u.size() == m_nnu);
+        GOOSEFEM_ASSERT(x_p.size() == m_nnp);
+        GOOSEFEM_ASSERT(b_u.size() == m_nnu);
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnu; ++d) {
+            b_u(d) = m_Auu(d) * x_u(d);
+        }
+    }
 
     /**
     \param x_u dofval [#nnu].
@@ -92,7 +234,12 @@ public:
     \return b_p dofval [#nnp].
     */
     array_type::tensor<double, 1>
-    Dot_p(const array_type::tensor<double, 1>& x_u, const array_type::tensor<double, 1>& x_p) const;
+    Dot_p(const array_type::tensor<double, 1>& x_u, const array_type::tensor<double, 1>& x_p) const
+    {
+        array_type::tensor<double, 1> b_p = xt::empty<double>({m_nnp});
+        this->dot_p(x_u, x_p, b_p);
+        return b_p;
+    }
 
     /**
     \param x_u dofval [#nnu].
@@ -102,7 +249,19 @@ public:
     void dot_p(
         const array_type::tensor<double, 1>& x_u,
         const array_type::tensor<double, 1>& x_p,
-        array_type::tensor<double, 1>& b_p) const;
+        array_type::tensor<double, 1>& b_p) const
+    {
+        UNUSED(x_u);
+
+        GOOSEFEM_ASSERT(x_u.size() == m_nnu);
+        GOOSEFEM_ASSERT(x_p.size() == m_nnp);
+        GOOSEFEM_ASSERT(b_p.size() == m_nnp);
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnp; ++d) {
+            b_p(d) = m_App(d) * x_p(d);
+        }
+    }
 
     /**
     Solve \f$ x_u = A_{uu}^{-1} (b_u - A_{up} * x_p) \equiv A_{uu}^{-1} b_u \f$.
@@ -110,7 +269,22 @@ public:
     \param b nodevec [#nelem, #ndim].
     \param x nodevec, modified with `x_u` [#nelem, #ndim].
     */
-    void solve(const array_type::tensor<double, 2>& b, array_type::tensor<double, 2>& x) override;
+    void solve(const array_type::tensor<double, 2>& b, array_type::tensor<double, 2>& x) override
+    {
+        GOOSEFEM_ASSERT(xt::has_shape(b, {m_nnode, m_ndim}));
+        GOOSEFEM_ASSERT(xt::has_shape(x, {m_nnode, m_ndim}));
+
+        this->factorize();
+
+#pragma omp parallel for
+        for (size_t m = 0; m < m_nnode; ++m) {
+            for (size_t i = 0; i < m_ndim; ++i) {
+                if (m_part(m, i) < m_nnu) {
+                    x(m, i) = m_inv_uu(m_part(m, i)) * b(m, i);
+                }
+            }
+        }
+    }
 
     /**
     Solve \f$ x_u = A_{uu}^{-1} (b_u - A_{up} * x_p) \equiv A_{uu}^{-1} b_u \f$.
@@ -118,7 +292,18 @@ public:
     \param b dofval [#ndof].
     \param x dofval, modified with `x_u` [#ndof].
     */
-    void solve(const array_type::tensor<double, 1>& b, array_type::tensor<double, 1>& x) override;
+    void solve(const array_type::tensor<double, 1>& b, array_type::tensor<double, 1>& x) override
+    {
+        GOOSEFEM_ASSERT(b.size() == m_ndof);
+        GOOSEFEM_ASSERT(x.size() == m_ndof);
+
+        this->factorize();
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnu; ++d) {
+            x(m_iiu(d)) = m_inv_uu(d) * b(m_iiu(d));
+        }
+    }
 
     /**
     \param b_u dofval [#nnu].
@@ -126,7 +311,12 @@ public:
     \return x_u dofval [#nnu].
     */
     array_type::tensor<double, 1>
-    Solve_u(const array_type::tensor<double, 1>& b_u, const array_type::tensor<double, 1>& x_p);
+    Solve_u(const array_type::tensor<double, 1>& b_u, const array_type::tensor<double, 1>& x_p)
+    {
+        array_type::tensor<double, 1> x_u = xt::empty<double>({m_nnu});
+        this->solve_u(b_u, x_p, x_u);
+        return x_u;
+    }
 
     /**
     \param b_u dofval [#nnu].
@@ -136,7 +326,21 @@ public:
     void solve_u(
         const array_type::tensor<double, 1>& b_u,
         const array_type::tensor<double, 1>& x_p,
-        array_type::tensor<double, 1>& x_u);
+        array_type::tensor<double, 1>& x_u)
+    {
+        UNUSED(x_p);
+
+        GOOSEFEM_ASSERT(b_u.size() == m_nnu);
+        GOOSEFEM_ASSERT(x_p.size() == m_nnp);
+        GOOSEFEM_ASSERT(x_u.size() == m_nnu);
+
+        this->factorize();
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnu; ++d) {
+            x_u(d) = m_inv_uu(d) * b_u(d);
+        }
+    }
 
     /**
     Get right-hand-size for corresponding to the prescribed DOFs.
@@ -150,7 +354,12 @@ public:
     \return Copy of `b` with \f$ b_p \f$ overwritten.
     */
     array_type::tensor<double, 2>
-    Reaction(const array_type::tensor<double, 2>& x, const array_type::tensor<double, 2>& b) const;
+    Reaction(const array_type::tensor<double, 2>& x, const array_type::tensor<double, 2>& b) const
+    {
+        array_type::tensor<double, 2> ret = b;
+        this->reaction(x, ret);
+        return ret;
+    }
 
     /**
     Same as Reaction(const array_type::tensor<double, 2>&, const array_type::tensor<double, 2>&),
@@ -159,7 +368,20 @@ public:
     \param x "nodevec" [#nnode, #ndim].
     \param b "nodevec" [#nnode, #ndim], \f$ b_p \f$ overwritten.
     */
-    void reaction(const array_type::tensor<double, 2>& x, array_type::tensor<double, 2>& b) const;
+    void reaction(const array_type::tensor<double, 2>& x, array_type::tensor<double, 2>& b) const
+    {
+        GOOSEFEM_ASSERT(xt::has_shape(x, {m_nnode, m_ndim}));
+        GOOSEFEM_ASSERT(xt::has_shape(b, {m_nnode, m_ndim}));
+
+#pragma omp parallel for
+        for (size_t m = 0; m < m_nnode; ++m) {
+            for (size_t i = 0; i < m_ndim; ++i) {
+                if (m_part(m, i) >= m_nnu) {
+                    b(m, i) = m_App(m_part(m, i) - m_nnu) * x(m, i);
+                }
+            }
+        }
+    }
 
     /**
     Same as Reaction(const array_type::tensor<double, 2>&, const array_type::tensor<double, 2>&),
@@ -170,7 +392,12 @@ public:
     \return Copy of `b` with \f$ b_p \f$ overwritten.
     */
     array_type::tensor<double, 1>
-    Reaction(const array_type::tensor<double, 1>& x, const array_type::tensor<double, 1>& b) const;
+    Reaction(const array_type::tensor<double, 1>& x, const array_type::tensor<double, 1>& b) const
+    {
+        array_type::tensor<double, 1> ret = b;
+        this->reaction(x, ret);
+        return ret;
+    }
 
     /**
     Same as Reaction(const array_type::tensor<double, 1>&, const array_type::tensor<double, 1>&),
@@ -179,8 +406,16 @@ public:
     \param x "dofval" [#ndof].
     \param b "dofval" [#ndof], \f$ b_p \f$ overwritten.
     */
-    void reaction(const array_type::tensor<double, 1>& x, array_type::tensor<double, 1>& b)
-        const; // modified with "b_p"
+    void reaction(const array_type::tensor<double, 1>& x, array_type::tensor<double, 1>& b) const
+    {
+        GOOSEFEM_ASSERT(x.size() == m_ndof);
+        GOOSEFEM_ASSERT(b.size() == m_ndof);
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnp; ++d) {
+            b(m_iip(d)) = m_App(d) * x(m_iip(d));
+        }
+    }
 
     /**
     Same as Reaction(const array_type::tensor<double, 1>&, const array_type::tensor<double, 1>&),
@@ -192,7 +427,12 @@ public:
     */
     array_type::tensor<double, 1> Reaction_p(
         const array_type::tensor<double, 1>& x_u,
-        const array_type::tensor<double, 1>& x_p) const;
+        const array_type::tensor<double, 1>& x_p) const
+    {
+        array_type::tensor<double, 1> b_p = xt::empty<double>({m_nnp});
+        this->reaction_p(x_u, x_p, b_p);
+        return b_p;
+    }
 
     /**
     Same as Reaction_p(const array_type::tensor<double, 1>&, const array_type::tensor<double, 1>&),
@@ -205,7 +445,19 @@ public:
     void reaction_p(
         const array_type::tensor<double, 1>& x_u,
         const array_type::tensor<double, 1>& x_p,
-        array_type::tensor<double, 1>& b_p) const;
+        array_type::tensor<double, 1>& b_p) const
+    {
+        UNUSED(x_u);
+
+        GOOSEFEM_ASSERT(x_u.size() == m_nnu);
+        GOOSEFEM_ASSERT(x_p.size() == m_nnp);
+        GOOSEFEM_ASSERT(b_p.size() == m_nnp);
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnp; ++d) {
+            b_p(d) = m_App(d) * x_p(d);
+        }
+    }
 
 private:
     // The diagonal matrix, and its inverse (re-used to solve different RHS)
@@ -223,11 +475,21 @@ private:
     size_t m_nnp; // number of prescribed DOFs
 
     // Compute inverse (automatically evaluated by "solve")
-    void factorize();
+    void factorize()
+    {
+        if (!m_changed) {
+            return;
+        }
+
+#pragma omp parallel for
+        for (size_t d = 0; d < m_nnu; ++d) {
+            m_inv_uu(d) = 1.0 / m_Auu(d);
+        }
+
+        m_changed = false;
+    }
 };
 
 } // namespace GooseFEM
-
-#include "MatrixDiagonalPartitioned.hpp"
 
 #endif
