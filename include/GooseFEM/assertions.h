ac8f80be363e8359808d75ca38112a766627585d
Tom de Geus <tom@geus.me>
2022-08-18 09:00:46 +0200

    [bugfix] `is_unique` gave a broadcast error, instead of returning `false` for non-unique data

--- include/GooseFEM/assertions.h
+++ include/GooseFEM/assertions.h
@@ -19,8 +19,15 @@ Returns true is a list is unique (has not duplicate items).
 template <class T>
 inline bool is_unique(const T& arg)
 {
-    array_type::tensor<typename T::value_type, 1> tmp = xt::ravel(arg);
-    return xt::all(xt::equal(xt::unique(tmp), xt::sort(tmp)));
+    array_type::tensor<typename T::value_type, 1> flat = xt::ravel(arg);
+    array_type::tensor<typename T::value_type, 1> unique = xt::unique(flat);
+    array_type::tensor<typename T::value_type, 1> sorted = xt::sort(flat);
+
+    if (unique.size() != sorted.size()) {
+        return false;
+    }
+
+    return xt::all(xt::equal(unique, sorted));
 }
 
 } // namespace GooseFEM
