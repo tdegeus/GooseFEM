bfdcd62ac8db6222c8934872518fe8a17cbfb770
Tom de Geus <tom@geus.me>
2022-08-26 19:14:38 +0200

    [docs] Fixing typo in configuration

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ author = "Tom de Geus"
 # ones.
 extensions = [
     "breathe",
-    "sphinx.ext.mathjax ",
+    "sphinx.ext.mathjax",
     "sphinx.ext.todo",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
